0658b1dc6caa83bbbcceb94ab49008b57945042a
Nikogda <camille.bausson@gmail.com>
2022-01-26 23:06:56 +0100

    Add ConsiderDependencies preanalyse's function test class

--- FlOpEDT/TTapp/tests/test_pre_analyse/test_slots_constraints.py
+++ FlOpEDT/TTapp/tests/test_pre_analyse/test_slots_constraints.py
@@ -8,6 +8,9 @@ from base.models import Week, Department
 from TTapp.TTConstraints.slots_constraints import ConsiderDependencies
 
 class ConsiderDependenciesTestCase(ConstraintTestCase):
+    # In this class, we consider the relation between TD and TP as TD has to be given before TP
+
+    fixtures = ['data_test_constraints.json']
 
     def setUp(self):
         # Set constraint's type
@@ -23,17 +26,63 @@ class ConsiderDependenciesTestCase(ConstraintTestCase):
         self.constraint_dep_2 = ConsiderDependencies.objects.get(department=self.dep_2)
 
         # Weeks
-        self.week_7_2022 = Week.objects.get(year=2022, nb=1)
+        self.week_13_2022 = Week.objects.get(year=2022, nb=13)
+        self.week_14_2022 = Week.objects.get(year=2022, nb=14)
+        self.week_15_2022 = Week.objects.get(year=2022, nb=15)
+        self.week_16_2022 = Week.objects.get(year=2022, nb=16)
+        self.week_17_2022 = Week.objects.get(year=2022, nb=17)
+        self.week_18_2022 = Week.objects.get(year=2022, nb=18)
 
-    def test(self):
+
+
+    def test_OK_case(self):
         # TODO
-        # Test 1
-        self.assertTrue(True)
+        # Test 1 : OK case
+        json_response_dict = self.constraint_default_dep.pre_analyse(week=self.week_13_2022)
+        self.assertJsonResponseIsOK("1", json_response_dict)
+        # Test 2 : OK case
+        json_response_dict = self.constraint_default_dep.pre_analyse(week=self.week_14_2022)
+        self.assertJsonResponseIsOK("2", json_response_dict)
+        # Test 3 : OK case
+        json_response_dict = self.constraint_default_dep.pre_analyse(week=self.week_15_2022)
+        self.assertJsonResponseIsOK("3", json_response_dict)
+
+    def test_consider_tutors_unavailabilities(self):
+        # Test 4 : KO case
+        json_response_dict = self.constraint_dep_2.pre_analyse(week=self.week_17_2022)
+        self.assertJsonResponseIsKO("4", json_response_dict)
+
+    def test_consider_courses_beginning_time(self):
+        # Test 5 : KO case
+        json_response_dict = self.constraint_default_dep.pre_analyse(week=self.week_16_2022)
+        self.assertJsonResponseIsKO("5", json_response_dict)
+
+    def test_consider_supp_tutors_unavailabilities(self):
+        # Test 6 : KO case : Tutor "bibiTU" has a TD and TP to give and he is available every day but monday
+        #          (when he is available only between 6 o'clock pm and 8 o'clock pm) and tutor "Prof1" is supp_tutor for
+        #          TP but he is only available on monday. So TP can not be ensured by "Prof1" before TD.
+        json_response_dict = self.constraint_dep_2.pre_analyse(week=self.week_18_2022)
+        self.assertJsonResponseIsKO("6", json_response_dict)
 
 # TODO : delete at the end, only print tests
 if __name__ == "__main__":
-    my_week = Week.objects.get(year=2022, nb=1)
-    dep = Department.objects.get(abbrev="Dept2")
-    constraint = ConsiderDependencies.objects.get(department=dep)
-    dico = constraint.pre_analyse(week=my_week)
+    my_week = Week.objects.get(year=2022, nb=18)
+    # Departments
+    default_dep = Department.objects.get(abbrev="default")
+    dep_2 = Department.objects.get(abbrev="Dept2")
+
+    # Constraints by departments
+    constraint_default_dep = ConsiderDependencies.objects.get(department=default_dep)
+    constraint_dep_2 = ConsiderDependencies.objects.get(department=dep_2)
+
+    dico = constraint_dep_2.pre_analyse(my_week)
     print(dico)
+
+    #dep = Department.objects.get(abbrev="Dept2")
+    #constraint = ConsiderDependencies.objects.get(department=dep)
+    #dico = constraint.pre_analyse(week=my_week)
+    #constraints = ConsiderDependencies.objects.all()
+    #for c in constraints:
+    #    dico = c.pre_analyse(week=my_week)
+    #    print(dico)
+    #    print(dep)
