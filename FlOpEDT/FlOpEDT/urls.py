943d97ad78e1950be096780a98c26e088c004b22
daskido <111780-daskido@users.noreply.framagit.org>
2022-10-14 19:22:45 +0200

    Update i18n

--- FlOpEDT/FlOpEDT/urls.py
+++ FlOpEDT/FlOpEDT/urls.py
@@ -90,8 +90,6 @@ urlpatterns += i18n_patterns(
 
 )
 
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
+# if settings.DEBUG:
+#     import debug_toolbar
+#     urlpatterns += [path('__debug__/', include(debug_toolbar.urls)),]
