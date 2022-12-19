acac7eaac47fa0df7bad9a3dfc2b4172f96c527b
PRG <prenaud@irit.fr>
2022-12-19 11:56:08 +0100

    (feat)upgrade revert stupid week converter

--- FlOpEDT/FlOpEDT/converters.py
+++ FlOpEDT/FlOpEDT/converters.py
@@ -6,12 +6,3 @@ class FourDigitYearConverter:
 
     def to_url(self, value):
         return '%04d' % value
-
-class TwoDigitYearConverter:
-    regex = '[0-9]{2}'
-
-    def to_python(self, value):
-        return int(value)
-
-    def to_url(self, value):
-        return '%02d' % value
