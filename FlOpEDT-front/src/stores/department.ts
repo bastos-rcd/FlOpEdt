069e5a35f4915c3d245ab3648cfd3aa69f2c7813
csl0779a <leo.cusseau@univ-tlse3.fr>
2023-02-14 21:00:48 +0100

    fix for no department in db

--- FlOpEDT-front/src/stores/department.ts
+++ FlOpEDT-front/src/stores/department.ts
@@ -17,6 +17,9 @@ export const useDepartmentStore = defineStore('dept', () => {
     async function fetchAllDepartments() : Promise<void> {
       await api?.getAllDepartments().then((json: any) => {
         departments.value = json
+        if(departments.value.length === 0) {
+          currentDepartment.value.id = 0
+        }
       })
     }
   
