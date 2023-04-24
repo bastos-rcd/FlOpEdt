1cf12bdbb8a643635568cc2365948a64a5144cca
Mathieu DARTIGUES <mathieu@dartic.fr>
2023-04-24 18:02:29 +0200

    [frontend] Add quasar calendar

--- FlOpEDT-front/src/histoire.setup.ts
+++ FlOpEDT-front/src/histoire.setup.ts
@@ -1,6 +1,9 @@
 import i18n from '@/i18n'
 import { defineSetupVue3 } from '@histoire/plugin-vue'
+import Plugin from '@quasar/quasar-ui-qcalendar/src/QCalendarDay.js'
+import '@quasar/quasar-ui-qcalendar/src/css/calendar-day.sass'
 
 export const setupVue3 = defineSetupVue3(({ app, story, variant }) => {
+  app.use(Plugin)
   app.use(i18n) 
 })
