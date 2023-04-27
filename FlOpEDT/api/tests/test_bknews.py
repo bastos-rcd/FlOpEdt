fa598a885f97fa4f1ff8e207ef37cfe99e44b558
PRG <prenaud@irit.fr>
2023-04-27 11:11:31 +0200

    [api-test] minor on dummy pytest example

--- FlOpEDT/api/tests/test_bknews.py
+++ FlOpEDT/api/tests/test_bknews.py
@@ -13,11 +13,10 @@ def department_networks(db) -> Department:
                                      name="Networks & Telecommunication")
 
 @pytest.fixture
-def y1(db, department_cs:Department) -> BreakingNews:
+def bknews1(db, department_cs:Department) -> BreakingNews:
     return BreakingNews.objects.create(department= department_cs,
                                        week = 11, year = 2021,
                                        y=5, txt = "Lorem ipsum dolor sit amet.")
-
 
 @pytest.fixture
 def client():
@@ -25,10 +24,11 @@ def client():
 
 # Query
 def test_bknews(client,
-                y1 : BreakingNews):
+                bknews : BreakingNews):
     endpoint = "/fr/api/display/breakingnews/"
-    response = client.get(endpoint, format="json")
+    response = client.get(endpoint)
     print(response)
+    assert response.status_code == 200
     print(response.data)
     assert len(response.data) == 1
     result = dict(response.data[0])
