d8af651a06f9f27a49cd9c70319b5876845d1fe4
Olivier Bonhomme <olivier@bonhomme.io>
2023-05-07 17:53:37 +0200

    Migrate directory creation from MyFlop to base application
    It makes more sense in base.

--- FlOpEDT/base/apps.py
+++ FlOpEDT/base/apps.py
@@ -31,3 +31,28 @@ class BaseConfig(AppConfig):
 
     def ready(self):
         import base.signals
+
+        # STARTUP code
+        # Create directory for serving static content in production
+        if not os.path.exists(ds.STATIC_ROOT):
+        
+            # Directory doesn't exist let's create it
+            print("Let's create %s" % ds.STATIC_ROOT)
+            os.makedirs(ds.STATIC_ROOT,exist_ok=True)
+        
+        # Create directory for django cache
+        if not os.path.exists(ds.CACHE_DIRECTORY):
+        
+            # Directory doesn't exist let's create it
+            print("Let's create %s" % ds.CACHE_DIRECTORY)
+            os.makedirs(ds.CACHE_DIRECTORY,exist_ok=True)
+        
+        # Create tmp directory used for solver resolution
+        if not os.path.exists(ds.TMP_DIRECTORY):
+        
+            # Directory doesn't exist let's create it
+            print("Let's create %s" % ds.TMP_DIRECTORY)
+            os.makedirs(ds.TMP_DIRECTORY,exist_ok=True)
+        
+        # Let's create the missing directories
+        os.makedirs(os.path.join(ds.TMP_DIRECTORY,"misc/logs/iis"),exist_ok=True)
