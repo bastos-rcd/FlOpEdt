5bb9a1b8fbf82cfa45d727ddc15f2b0001b7bd88
PRG <prenaud@irit.fr>
2023-05-07 17:40:09 +0200

    test(tree) clean up the tests

--- FlOpEDT-front/src/ts/tree.spec.ts
+++ FlOpEDT-front/src/ts/tree.spec.ts
@@ -1,15 +1,15 @@
 import { TreeNode, Tree } from './tree'
 import { describe, expect, it } from 'vitest'
-import { map } from 'lodash'
+import { map, reverse } from 'lodash'
 
 describe('Tree utils', () => {
   it('stores a reasonable list', () => {
     expect.assertions(4)
-    const tree = new Tree<{id:number, pouet:string, parent:number|null}>()
+    const tree = new Tree()
     tree.addNodes([
-        {id: 5, pouet: "truc5", parent:1},
-        {id: 1, pouet: "truc1", parent:null},
-        {id: 2, pouet: "truc2", parent:1},
+        {id: 5, parentId:1},
+        {id: 1, parentId:null},
+        {id: 2, parentId:1},
     ])
     expect(Object.keys(tree.byId).length).toBe(3)
     expect(tree.byId[5]).toBeDefined()
@@ -19,83 +19,89 @@ describe('Tree utils', () => {
 
   it('builds correct children and parents', () => {
     expect.assertions(9)
-    const tree = new Tree<{id:number, pouet:string, parent:number|null}>()
+    const tree = new Tree()
     tree.addNodes([
-        {id: 5, pouet: "truc5", parent:1},
-        {id: 1, pouet: "truc1", parent:null},
-        {id: 2, pouet: "truc2", parent:1},
-        {id: 8, pouet: "truc8", parent:5}
+        {id: 5, parentId:1},
+        {id: 1, parentId:null},
+        {id: 2, parentId:1},
+        {id: 8, parentId:5}
     ])
     expect(tree.byId[1].children.length).toBe(2)
-    expect(map(tree.byId[1].children, c => c.data.id)).toEqual(expect.arrayContaining([5, 2]))
+    expect(map(tree.byId[1].children, c => c.id)).toEqual(expect.arrayContaining([5, 2]))
     expect(tree.byId[2].children.length).toBe(0)
-    expect(map(tree.byId[5].children, c => c.data.id)).toEqual([8])
+    expect(map(tree.byId[5].children, c => c.id)).toEqual([8])
     expect(tree.byId[5].parent).toBe(tree.byId[1])
 
-    expect(tree.byId[5].parent?.data.id).toBe(1)
+    expect(tree.byId[5].parent?.id).toBe(1)
     expect(tree.byId[1].parent).toBeNull()
-    expect(tree.byId[2].parent?.data.id).toBe(1)
-    expect(tree.byId[8].parent?.data.id).toBe(5)
+    expect(tree.byId[2].parent?.id).toBe(1)
+    expect(tree.byId[8].parent?.id).toBe(5)
   })
 
   it('builds correct descendants', () => {
     expect.assertions(18)
-    const tree = new Tree<{id:number, pouet:string, parent:number|null}>()
+    const tree = new Tree()
     tree.addNodes([
-        {id: 5, pouet: "truc5", parent:1},
-        {id: 1, pouet: "truc1", parent:null},
-        {id: 2, pouet: "truc2", parent:1},
-        {id: 8, pouet: "truc8", parent:5}
+        {id: 5, parentId:1},
+        {id: 1, parentId:null},
+        {id: 2, parentId:1},
+        {id: 8, parentId:5}
     ])
 
     expect(tree.byId[1].children.length).toBe(2)
-    expect(map(tree.byId[1].children, c => c.data.id)).toEqual(expect.arrayContaining([5, 2]))
+    expect(map(tree.byId[1].children, c => c.id)).toEqual(expect.arrayContaining([5, 2]))
     expect(tree.byId[2].children.length).toBe(0)
-    expect(map(tree.byId[5].children, c => c.data.id)).toEqual([8])
+    expect(map(tree.byId[5].children, c => c.id)).toEqual([8])
     expect(tree.byId[5].parent).toBe(tree.byId[1])
 
     expect(tree.byId[1].descendants.length).toBe(3)
-    expect(map(tree.byId[1].descendants, c => c.data.id)).toEqual(expect.arrayContaining([2, 5, 8]))
+    expect(map(tree.byId[1].descendants, c => c.id)).toEqual(expect.arrayContaining([2, 5, 8]))
     expect(tree.byId[1].ancestors.length).toBe(0)
     expect(tree.byId[5].descendants.length).toBe(1)
-    expect(map(tree.byId[5].descendants, c => c.data.id)).toEqual(expect.arrayContaining([8]))
+    expect(map(tree.byId[5].descendants, c => c.id)).toEqual(expect.arrayContaining([8]))
     expect(tree.byId[5].ancestors.length).toBe(1)
-    expect(map(tree.byId[5].ancestors, c => c.data.id)).toEqual(expect.arrayContaining([1]))
+    expect(map(tree.byId[5].ancestors, c => c.id)).toEqual(expect.arrayContaining([1]))
     expect(tree.byId[8].descendants.length).toBe(0)
     expect(tree.byId[8].ancestors.length).toBe(2)
-    expect(map(tree.byId[8].ancestors, c => c.data.id)).toEqual(expect.arrayContaining([1, 5]))
+    expect(map(tree.byId[8].ancestors, c => c.id)).toEqual(expect.arrayContaining([1, 5]))
     expect(tree.byId[2].descendants.length).toBe(0)
     expect(tree.byId[2].ancestors.length).toBe(1)
-    expect(map(tree.byId[2].ancestors, c => c.data.id)).toEqual(expect.arrayContaining([1]))
+    expect(map(tree.byId[2].ancestors, c => c.id)).toEqual(expect.arrayContaining([1]))
 
   })
 
   it('sorts the children correctly', () => {
-    expect.assertions(3)
-    const tree = new Tree<{id:number, pouet:string, parent:number|null}>()
-    tree.addNodes([
-        {id: 5, pouet: 2, parent:1},
-        {id: 1, pouet: 1, parent:null},
-        {id: 2, pouet: 5, parent:1},
-        {id: 8, pouet: 8, parent:5}
-    ])
-    tree.sortChildrenBy("id")
-    expect(map(tree.byId[1].children, c => c.data.id)).toEqual([2, 5])
-    tree.sortChildrenBy("pouet")
-    expect(map(tree.byId[1].children, c => c.data.id)).toEqual([5, 2])
-    tree.sortChildrenBy("aze")
-    expect(map(tree.byId[1].children, c => c.data.id)).toEqual([5, 2])
+    expect.assertions(2)
+    const tree = new Tree()
+
+    const linkIdUps = [
+      {id: 5, parentId:1},
+      {id: 1, parentId:null},
+      {id: 2, parentId:1},
+      {id: 8, parentId:5}
+    ]
+
+    tree.addNodes(linkIdUps)
+    tree.sortChildren()
+    expect(map(tree.byId[1].children, c => c.id)).toEqual([5, 2])
+
+    const reverseTree = new Tree()
+    reverse(linkIdUps)
+    reverseTree.addNodes(linkIdUps)
+    reverseTree.sortChildren()
+    expect(map(reverseTree.byId[1].children, c => c.id)).toEqual([2, 5])
+    
   })
 
 
   it("computes correctly levels", () => {
     expect.assertions(8)
-    const tree = new Tree<{id:number, pouet:string, parent:number|null}>()
+    const tree = new Tree()
     tree.addNodes([
-        {id: 5, pouet: 2, parent:1},
-        {id: 1, pouet: 1, parent:null},
-        {id: 2, pouet: 5, parent:1},
-        {id: 8, pouet: 8, parent:5}
+        {id: 5, parentId:1},
+        {id: 1, parentId:null},
+        {id: 2, parentId:1},
+        {id: 8, parentId:5}
     ])
     tree.computeDepthMin()
     expect(tree.byId[1].depthMin).toBe(0)
@@ -111,13 +117,13 @@ describe('Tree utils', () => {
   })
   it("computes correctly the number of leaves", () => {
     expect.assertions(5)
-    const tree = new Tree<{id:number, pouet:string, parent:number|null}>()
+    const tree = new Tree()
     tree.addNodes([
-        {id: 5, pouet: 2, parent:1},
-        {id: 1, pouet: 1, parent:null},
-        {id: 2, pouet: 5, parent:1},
-        {id: 8, pouet: 8, parent:5},
-        {id: 3, pouet: 3, parent:5}
+        {id: 5, parentId:1},
+        {id: 1, parentId:null},
+        {id: 2, parentId:1},
+        {id: 8, parentId:5},
+        {id: 3, parentId:5}
     ])
     tree.countLeaves()
     expect(tree.byId[1].nLeaves).toBe(3)
@@ -132,12 +138,12 @@ describe('Tree utils', () => {
 
   it.todo('throws an error when cycle')
 
-  it('throws an error when unknown node as a parent', () => {
+  it('throws an error when unknown node as a parentId', () => {
     expect.assertions(1)
     const tree = new Tree()
     expect(() => tree.addNodes([
-        {id: 1, pouet: "truc", parent:null},
-        {id: 2, pouet: "truc", parent:3},
+        {id: 1, parentId:null},
+        {id: 2, parentId:3},
     ])).toThrowError('unknown')
   })
 
@@ -145,9 +151,9 @@ describe('Tree utils', () => {
     expect.assertions(1)
     const tree = new Tree()
     expect(() => tree.addNodes([
-        {id: 1, pouet: "machin", parent:null},
-        {id: 2, pouet: "truc", parent:1},
-        {id: 5, pouet: "trucz", parent:null},
+        {id: 1, parentId:null},
+        {id: 2, parentId:1},
+        {id: 5, parentId:null},
     ])).toThrowError('multi')
   })
 })
