96d98773753a5ab5ef1dd9da9cd2805639a9642a
PRG <prenaud@irit.fr>
2023-05-08 22:44:04 +0200

    test(dyn-col) test active property of nodes

--- FlOpEDT-front/src/ts/tree.spec.ts
+++ FlOpEDT-front/src/ts/tree.spec.ts
@@ -1,6 +1,6 @@
 import { TreeNode, Tree } from './tree'
 import { describe, expect, it } from 'vitest'
-import { map, reverse } from 'lodash'
+import { map, reverse, filter, values } from 'lodash'
 
 describe('Tree utils', () => {
   it('stores a reasonable list', () => {
@@ -10,7 +10,7 @@ describe('Tree utils', () => {
         {id: 5, parentId:1},
         {id: 1, parentId:null},
         {id: 2, parentId:1},
-    ])
+    ],[])
     expect(Object.keys(tree.byId).length).toBe(3)
     expect(tree.byId[5]).toBeDefined()
     expect(tree.byId[1]).toBeDefined()
@@ -25,7 +25,7 @@ describe('Tree utils', () => {
         {id: 1, parentId:null},
         {id: 2, parentId:1},
         {id: 8, parentId:5}
-    ])
+    ],[])
     expect(tree.byId[1].children.length).toBe(2)
     expect(map(tree.byId[1].children, c => c.id)).toEqual(expect.arrayContaining([5, 2]))
     expect(tree.byId[2].children.length).toBe(0)
@@ -46,7 +46,7 @@ describe('Tree utils', () => {
         {id: 1, parentId:null},
         {id: 2, parentId:1},
         {id: 8, parentId:5}
-    ])
+    ],[])
 
     expect(tree.byId[1].children.length).toBe(2)
     expect(map(tree.byId[1].children, c => c.id)).toEqual(expect.arrayContaining([5, 2]))
@@ -81,13 +81,13 @@ describe('Tree utils', () => {
       {id: 8, parentId:5}
     ]
 
-    tree.addNodes(linkIdUps)
+    tree.addNodes(linkIdUps,[])
     tree.sortChildren()
     expect(map(tree.byId[1].children, c => c.id)).toEqual([5, 2])
 
     const reverseTree = new Tree()
     reverse(linkIdUps)
-    reverseTree.addNodes(linkIdUps)
+    reverseTree.addNodes(linkIdUps,[])
     reverseTree.sortChildren()
     expect(map(reverseTree.byId[1].children, c => c.id)).toEqual([2, 5])
     
@@ -102,7 +102,7 @@ describe('Tree utils', () => {
         {id: 1, parentId:null},
         {id: 2, parentId:1},
         {id: 8, parentId:5}
-    ])
+    ],[])
     tree.computeDepthMin()
     expect(tree.byId[1].depthMin).toBe(0)
     expect(tree.byId[5].depthMin).toBe(1)
@@ -124,7 +124,7 @@ describe('Tree utils', () => {
         {id: 2, parentId:1},
         {id: 8, parentId:5},
         {id: 3, parentId:5}
-    ])
+    ],[])
     tree.countLeaves()
     expect(tree.byId[1].nLeaves).toBe(3)
     expect(tree.byId[5].nLeaves).toBe(2)
@@ -136,6 +136,182 @@ describe('Tree utils', () => {
   })
 
 
+
+  it("activates the right nodes", () => {
+    expect.assertions(5)
+    const tree = new Tree()
+    tree.addNodes([
+        {id: 5, parentId:1},
+        {id: 1, parentId:null},
+        {id: 2, parentId:1},
+        {id: 8, parentId:5},
+        {id: 3, parentId:5}
+    ], [3])
+
+    expect(tree.byId[3].active).toBe(true)
+    expect(tree.byId[5].active).toBe(true)
+    expect(tree.byId[1].active).toBe(true)
+    expect(tree.byId[8].active).toBe(false)
+    expect(tree.byId[2].active).toBe(false)
+
+  })
+
+  it("activates the right nodes (bis)", () => {
+    expect.assertions(2)
+    const tree = new Tree()
+    tree.addNodes([
+      {id: 1,   parentId:null},
+      {id: 11,  parentId:1},
+      {id: 12,  parentId:1},
+      {id: 13,  parentId:1},
+      {id: 111, parentId:11},
+      {id: 112, parentId:11},
+      {id: 121, parentId:12},
+      {id: 122, parentId:12},
+      {id: 131, parentId:13},
+      {id: 132, parentId:13}
+        ], [131, 121])
+
+      let actives = map(filter(values(tree.byId), node => node.active),
+      node => node.id)
+      expect(actives.length).toBe(5)
+      expect(actives).toEqual(expect.arrayContaining([1, 12,13,121,131]))
+
+  })
+
+  it("activates the right nodes (ter)", () => {
+    expect.assertions(4)
+    const tree = new Tree()
+    tree.addNodes([
+      {id: 1,   parentId:null},
+      {id: 11,  parentId:1},
+      {id: 12,  parentId:1},
+      {id: 13,  parentId:1},
+      {id: 111, parentId:11},
+      {id: 112, parentId:11},
+      {id: 121, parentId:12},
+      {id: 122, parentId:12},
+      {id: 131, parentId:13},
+      {id: 132, parentId:13}
+        ], [ 1, 11, 12, 112, 122 ])
+
+    let actives = map(filter(values(tree.byId), node => node.active),
+    node => node.id)
+    expect(actives.length).toBe(5)
+    expect(actives).toEqual(expect.arrayContaining([ 1, 11, 12, 112, 122 ]))
+
+    tree.byId[11].toggleActive()
+    actives = map(filter(values(tree.byId), node => node.active),
+    node => node.id)
+    expect(actives.length).toBe(3)
+    expect(actives).toEqual(expect.arrayContaining([ 1, 12, 122 ]))
+
+  })
+
+  // 
+
+  it("reacts correctly to activation", () => {
+    expect.assertions(6)
+    const tree = new Tree()
+    tree.addNodes([
+        {id: 5, parentId:1},
+        {id: 1, parentId:null},
+        {id: 2, parentId:1},
+        {id: 8, parentId:5},
+        {id: 3, parentId:5},
+        {id: 4, parentId:2},
+        {id: 6, parentId:2},
+        {id:11, parentId:1},
+        {id:12, parentId:11},
+        {id:13, parentId:11}
+    ], [3])
+
+    let actives = map(filter(values(tree.byId), node => node.active),
+      node => node.id)
+    expect(actives.length).toBe(3)
+    expect(actives).toEqual(expect.arrayContaining([1, 3, 5]))
+
+    tree.byId[4].toggleActive()
+
+    actives = map(filter(values(tree.byId), node => node.active),
+    node => node.id)
+    expect(actives.length).toBe(5)
+    expect(actives).toEqual(expect.arrayContaining([1, 3, 5, 4, 2]))
+
+    tree.byId[11].toggleActive()
+
+    actives = map(filter(values(tree.byId), node => node.active),
+    node => node.id)
+    expect(actives.length).toBe(8)
+    expect(actives).toEqual(expect.arrayContaining([1, 3, 5, 4, 2, 11, 12, 13]))
+  })
+
+  it("reacts correctly to deactivation", () => {
+    expect.assertions(6)
+    const tree = new Tree()
+    tree.addNodes([
+        {id: 5, parentId:1},
+        {id: 1, parentId:null},
+        {id: 2, parentId:1},
+        {id: 8, parentId:5},
+        {id: 3, parentId:5},
+        {id: 4, parentId:2},
+        {id: 6, parentId:2},
+        {id:11, parentId:1},
+        {id:12, parentId:11},
+        {id:13, parentId:11}
+    ], [3, 4, 6])
+
+    let actives = map(filter(values(tree.byId), node => node.active),
+    node => node.id)
+    expect(actives.length).toBe(6)
+    expect(actives).toEqual(expect.arrayContaining([1, 3, 5, 2, 4, 6]))
+
+    tree.byId[4].toggleActive()
+
+    actives = map(filter(values(tree.byId), node => node.active),
+    node => node.id)
+    expect(actives.length).toBe(5)
+    expect(actives).toEqual(expect.arrayContaining([1, 3, 5, 2, 6]))
+  
+    tree.byId[5].toggleActive()
+
+    actives = map(filter(values(tree.byId), node => node.active),
+    node => node.id)
+    expect(actives.length).toBe(3)
+    expect(actives).toEqual(expect.arrayContaining([1, 2, 6]))
+
+  })
+
+  it("activates correctly when all activated", () => {
+    expect.assertions(3)
+    const tree = new Tree()
+    tree.addNodes([
+        {id: 5, parentId:1},
+        {id: 1, parentId:null},
+        {id: 2, parentId:1},
+        {id: 8, parentId:5},
+        {id: 3, parentId:5},
+        {id: 4, parentId:2},
+        {id: 6, parentId:2},
+        {id:11, parentId:1},
+        {id:12, parentId:11},
+        {id:13, parentId:11}
+    ], [8, 3, 4, 6, 12, 13])
+    let actives = map(filter(values(tree.byId), node => node.active),
+    node => node.id)
+    expect(actives.length).toBe(values(tree.byId).length)
+    
+    tree.byId[2].toggleActive()
+
+    actives = map(filter(values(tree.byId), node => node.active),
+    node => node.id)
+    expect(actives.length).toBe(4)
+    expect(actives).toEqual(expect.arrayContaining([1, 2, 4, 6]))
+  
+
+  })
+
   it.todo('throws an error when cycle')
 
   it('throws an error when unknown node as a parentId', () => {
@@ -144,7 +320,7 @@ describe('Tree utils', () => {
     expect(() => tree.addNodes([
         {id: 1, parentId:null},
         {id: 2, parentId:3},
-    ])).toThrowError('unknown')
+    ],[])).toThrowError('unknown')
   })
 
   it('throws an error when multi-root', () => {
@@ -154,6 +330,6 @@ describe('Tree utils', () => {
         {id: 1, parentId:null},
         {id: 2, parentId:1},
         {id: 5, parentId:null},
-    ])).toThrowError('multi')
+    ],[])).toThrowError('multi')
   })
 })
