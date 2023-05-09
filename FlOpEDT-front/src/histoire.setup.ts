582ac9e2eaab45b21f4ac6e11d6e04ff7cad640b
csl0779a <leo.cusseau@univ-tlse3.fr>
2023-05-09 23:25:07 +0200

    feat(): adding quasar to project

--- FlOpEDT-front/src/histoire.setup.ts
+++ FlOpEDT-front/src/histoire.setup.ts
@@ -3,10 +3,16 @@ import { createPinia } from 'pinia'
 import { defineSetupVue3 } from '@histoire/plugin-vue'
 import QcalendarPlugin from '@quasar/quasar-ui-qcalendar/src/QCalendarDay.js'
 import '@quasar/quasar-ui-qcalendar/src/css/calendar-day.sass'
+import { Quasar } from 'quasar'
+import '@quasar/extras/material-icons/material-icons.css'
+import 'quasar/src/css/index.sass'
 
 export const setupVue3 = defineSetupVue3(({ app, story, variant }) => {
   const pinia = createPinia()
   app.use(pinia)
   app.use(QcalendarPlugin)
-  app.use(i18n) 
+  app.use(i18n)
+  app.use(Quasar, {
+    plugins: {},
+  })
 })
--- FlOpEDT-front/vite.config.ts
+++ FlOpEDT-front/vite.config.ts
@@ -1,43 +1,44 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import * as path from 'path'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
-    resolve: {
-        alias: {
-                '@': path.resolve(__dirname, 'src'),
-                '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-        },
+  plugins: [vue( {template: {transformAssetUrls}} ), quasar({sassVariables: 'src/quasar-variables.sass'})],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
     },
-    server: {
-        proxy: {
-            '/fr': {
-                target: 'http://127.0.0.1:8000',
-                changeOrigin: true,
-            },
-            '/en': {
-                target: 'http://127.0.0.1:8000',
-                changeOrigin: true,
-            },
-            '/es': {
-                target: 'http://127.0.0.1:8000',
-                changeOrigin: true,
-            },
-            '/static': {
-                target: 'http://127.0.0.1:8000',
-                changeOrigin: true,
-            },
-            '/api': {
-                target: 'http://127.0.0.1:8000',
-                changeOrigin: true,
-            },
-        },
+  },
+  server: {
+    proxy: {
+      '/fr': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+      },
+      '/en': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+      },
+      '/es': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+      },
+      '/static': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+      },
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+      },
     },
-    define: {
-        __VUE_I18N_FULL_INSTALL__: true,
-        __VUE_I18N_LEGACY_API__: false,
-        __INTLIFY_PROD_DEVTOOLS__: false,
-    },
+  },
+  define: {
+    __VUE_I18N_FULL_INSTALL__: true,
+    __VUE_I18N_LEGACY_API__: false,
+    __INTLIFY_PROD_DEVTOOLS__: false,
+  },
 })
