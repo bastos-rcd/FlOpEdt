7b6898ec9142f57ad8420eef63377f9d414ddf0b
PRG <prenaud@irit.fr>
2023-05-11 09:28:30 +0200

    lint(dyn-col) prettier

--- FlOpEDT-front/src/components/qcalendar/HierarchicalColumnFilter.story.vue
+++ FlOpEDT-front/src/components/qcalendar/HierarchicalColumnFilter.story.vue
@@ -1,63 +1,51 @@
 <template>
-    <Story>
-       <Variant :title="useCase1.title">
-          <HierarchicalColumnFilter
-            :active-ids="[]"
-            :flatNodes="useCase1.flatNodes">
-              <template #item="{ nodeId }">
-                {{ find(useCase1.flatNodes, n => n.id === nodeId)?.name }}
-              </template>
-          </HierarchicalColumnFilter>
-      </Variant>
-      <Variant title="Default slot">
-          <HierarchicalColumnFilter
-            :active-ids="[]"
-            :flatNodes="useCase2"
-          />
-      </Variant>
-      <Variant title="Classical shape">
-        <HierarchicalColumnFilter
-            :active-ids="[]"
-            :flatNodes="useCase3">
-              <template #item="{ nodeId }">
-                {{ find(useCase3, n => n.id === nodeId)?.name }}
-              </template>
-          </HierarchicalColumnFilter>
-      </Variant>
-      <Variant title="Interactive inside component">
-        <HierarchicalColumnFilter
-            v-model:active-ids="activeNodeIds"
-            :flatNodes="useCase4.flatNodes">
-            <!-- @update:activeIds="updateInPlace"> -->
-              <template #item="{ nodeId, active }">
-                <div :class="['node', active?'ac':'nac']">
-                  {{ find(useCase4.flatNodes, n => (n.id === nodeId))?.name }}
-                  {{ active }}
-                </div>
-              </template>
-          </HierarchicalColumnFilter>
-      </Variant>
-      <Variant title="Interactive from outside">
-        <HierarchicalColumnFilter
-            v-model:active-ids="activeNodeIds"
-            :flatNodes="useCase4.flatNodes">
-              <template #item="{ nodeId, active }">
-                <div :class="['node', active?'ac':'nac']">
-                  {{ find(useCase4.flatNodes, n => (n.id === nodeId))?.name }}
-                  {{ active }}
-                </div>
-              </template>
-          </HierarchicalColumnFilter>
-          <div>
-            {{ activeNodeIds }}
+  <Story>
+    <Variant :title="useCase1.title">
+      <HierarchicalColumnFilter :active-ids="[]" :flatNodes="useCase1.flatNodes">
+        <template #item="{ nodeId }">
+          {{ find(useCase1.flatNodes, (n) => n.id === nodeId)?.name }}
+        </template>
+      </HierarchicalColumnFilter>
+    </Variant>
+    <Variant title="Default slot">
+      <HierarchicalColumnFilter :active-ids="[]" :flatNodes="useCase2" />
+    </Variant>
+    <Variant title="Classical shape">
+      <HierarchicalColumnFilter :active-ids="[]" :flatNodes="useCase3">
+        <template #item="{ nodeId }">
+          {{ find(useCase3, (n) => n.id === nodeId)?.name }}
+        </template>
+      </HierarchicalColumnFilter>
+    </Variant>
+    <Variant title="Interactive inside component">
+      <HierarchicalColumnFilter v-model:active-ids="activeNodeIds" :flatNodes="useCase4.flatNodes">
+        <!-- @update:activeIds="updateInPlace"> -->
+        <template #item="{ nodeId, active }">
+          <div :class="['node', active ? 'ac' : 'nac']">
+            {{ find(useCase4.flatNodes, (n) => n.id === nodeId)?.name }}
+            {{ active }}
           </div>
-          <template v-for="nid in [111,112,121,122,131,132]">
-            <button @click="deactivate(nid)">Deactivate {{ find(useCase4.flatNodes, n => n.id == nid)?.name }}</button>
-          </template>
-      </Variant>
-    </Story>
+        </template>
+      </HierarchicalColumnFilter>
+    </Variant>
+    <Variant title="Interactive from outside">
+      <HierarchicalColumnFilter v-model:active-ids="activeNodeIds" :flatNodes="useCase4.flatNodes">
+        <template #item="{ nodeId, active }">
+          <div :class="['node', active ? 'ac' : 'nac']">
+            {{ find(useCase4.flatNodes, (n) => n.id === nodeId)?.name }}
+            {{ active }}
+          </div>
+        </template>
+      </HierarchicalColumnFilter>
+      <div>
+        {{ activeNodeIds }}
+      </div>
+      <template v-for="nid in [111, 112, 121, 122, 131, 132]">
+        <button @click="deactivate(nid)">Deactivate {{ find(useCase4.flatNodes, (n) => n.id == nid)?.name }}</button>
+      </template>
+    </Variant>
+  </Story>
 </template>
-  
 
 <script setup lang="ts">
 import HierarchicalColumnFilter from './HierarchicalColumnFilter.vue'
@@ -66,108 +54,113 @@ import { find, forEach, remove, filter } from 'lodash'
 import { ref } from 'vue'
 
 const useCase1 = {
-  title: "Named with slots",
+  title: 'Named with slots',
   flatNodes: [
-    {id: 5, name: "id5", parentId:1},
-    {id: 1, name: "id1", parentId:null},
-    {id: 2, name: "id2", parentId:1},
-    {id: 8, name: "id8", parentId:5}
-  ]
+    { id: 5, name: 'id5', parentId: 1 },
+    { id: 1, name: 'id1', parentId: null },
+    { id: 2, name: 'id2', parentId: 1 },
+    { id: 8, name: 'id8', parentId: 5 },
+  ],
 }
 
 const useCase2 = [
-    {id: 5, name: "id5", parentId:1},
-    {id: 1, name: "id1", parentId:null},
-    {id: 2, name: "id2", parentId:1},
-    {id: 3, name: "id3", parentId:1},
-    {id: 8, name: "id8", parentId:5},
-    {id: 9, name: "id9", parentId:5}
+  { id: 5, name: 'id5', parentId: 1 },
+  { id: 1, name: 'id1', parentId: null },
+  { id: 2, name: 'id2', parentId: 1 },
+  { id: 3, name: 'id3', parentId: 1 },
+  { id: 8, name: 'id8', parentId: 5 },
+  { id: 9, name: 'id9', parentId: 5 },
 ]
 const useCase3 = [
-    {id: 1, name: "CE", parentId:null},
-    {id: 11, name: "TD1", parentId:1},
-    {id: 12, name: "TD2", parentId:1},
-    {id: 13, name: "TD3", parentId:1},
-    {id: 111, name: "TP1A", parentId:11},
-    {id: 112, name: "TP1B", parentId:11},
-    {id: 121, name: "TP2A", parentId:12},
-    {id: 122, name: "TP2B", parentId:12},
-    {id: 131, name: "TP3A", parentId:13},
-    {id: 132, name: "TP3B", parentId:13}
+  { id: 1, name: 'CE', parentId: null },
+  { id: 11, name: 'TD1', parentId: 1 },
+  { id: 12, name: 'TD2', parentId: 1 },
+  { id: 13, name: 'TD3', parentId: 1 },
+  { id: 111, name: 'TP1A', parentId: 11 },
+  { id: 112, name: 'TP1B', parentId: 11 },
+  { id: 121, name: 'TP2A', parentId: 12 },
+  { id: 122, name: 'TP2B', parentId: 12 },
+  { id: 131, name: 'TP3A', parentId: 13 },
+  { id: 132, name: 'TP3B', parentId: 13 },
 ]
 
 const activeNodeIds = ref([131, 121])
 
 const useCase4 = {
   flatNodes: [
-    {id: 1, name: "CE", parentId:null},
-    {id: 11, name: "TD1", parentId:1},
-    {id: 12, name: "TD2", parentId:1},
-    {id: 13, name: "TD3", parentId:1},
-    {id: 111, name: "TP1A", parentId:11},
-    {id: 112, name: "TP1B", parentId:11},
-    {id: 121, name: "TP2A", parentId:12},
-    {id: 122, name: "TP2B", parentId:12},
-    {id: 131, name: "TP3A", parentId:13},
-    {id: 132, name: "TP3B", parentId:13}
-  ]
+    { id: 1, name: 'CE', parentId: null },
+    { id: 11, name: 'TD1', parentId: 1 },
+    { id: 12, name: 'TD2', parentId: 1 },
+    { id: 13, name: 'TD3', parentId: 1 },
+    { id: 111, name: 'TP1A', parentId: 11 },
+    { id: 112, name: 'TP1B', parentId: 11 },
+    { id: 121, name: 'TP2A', parentId: 12 },
+    { id: 122, name: 'TP2B', parentId: 12 },
+    { id: 131, name: 'TP3A', parentId: 13 },
+    { id: 132, name: 'TP3B', parentId: 13 },
+  ],
 }
 
 function deactivate(id: number) {
-  activeNodeIds.value = filter(activeNodeIds.value, nid => (nid!=id))
+  activeNodeIds.value = filter(activeNodeIds.value, (nid) => nid != id)
 }
-
 </script>
 
 <style>
 .node {
-  height: 100% ;
-  width: 100% ;
+  height: 100%;
+  width: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
 }
 .ac {
-  background-color:rgba(25, 124, 25, 0.685)
+  background-color: rgba(25, 124, 25, 0.685);
 }
 .nac {
-  background-color:rgb(133, 34, 34)
+  background-color: rgb(133, 34, 34);
 }
 /* style="{width: 100 + '%'}" :style="active?{backgroundColor: 'green', height: 100 + '%'}:{backgroundColor: 'red'}" */
 </style>
 
 <docs lang="md">
-  # Welcome to a not-that-good doc
+# Welcome to a not-that-good doc
 
-  ## Technical use
-  ### Slots
-  `HierarchicalColumnFilter` component creates scoped slots, named `item`, whose scope is in
-  the form of `{nodeId: number, active: boolean}`. The slot content will be centered and
-  stretched inside the placeholder.
+## Technical use
 
-  **Note:** the scope is reactive.
+### Slots
 
-  ### Input/output
-  Clean `v-model` (I think).
-  - (Change in component => change in the caller environment) : see "Interactive inside component"
-  - (Change out of the component => change in the callee environment) : see "Interactive outside component"
+`HierarchicalColumnFilter` component creates scoped slots, named `item`, whose scope is in
+the form of `{nodeId: number, active: boolean}`. The slot content will be centered and
+stretched inside the placeholder.
 
-  **Note:** as the caller can pass an inconsistent list of active nodes
-  (*e.g.* an inactive parent node with children that are all active), we only
-  take into account in `props.activeNodeIds` the leaf nodes. The inconsistent
-  list in the caller component is changed by the `HierarchicalColumnFilter`
-  component into a consistent list.
+**Note:** the scope is reactive.
 
-  ## User interface
-  - Click on a node => toggle activation (except specific case where all nodes are active)
-  - "Nobody is active" is a transient state that resolves into "Everybody is active"
+### Input/output
 
-  ## TODO
-  - The aesthetics should be improved (gaps, remove the tiny margins that appear in blue),
+Clean `v-model` (I think).
+
+- (Change in component => change in the caller environment) : see "Interactive inside component"
+- (Change out of the component => change in the callee environment) : see "Interactive outside component"
+
+**Note:** as the caller can pass an inconsistent list of active nodes
+(_e.g._ an inactive parent node with children that are all active), we only
+take into account in `props.activeNodeIds` the leaf nodes. The inconsistent
+list in the caller component is changed by the `HierarchicalColumnFilter`
+component into a consistent list.
+
+## User interface
+
+- Click on a node => toggle activation (except specific case where all nodes are active)
+- "Nobody is active" is a transient state that resolves into "Everybody is active"
+
+## TODO
+
+- The aesthetics should be improved (gaps, remove the tiny margins that appear in blue),
   and I guess slots should help.
-  - The style may not be clean (I'm quite ignorant for now): should we scope something?
-  
-  ---
-  
-  Learn more about Histoire [here](https://histoire.dev/).
-  </docs>
+- The style may not be clean (I'm quite ignorant for now): should we scope something?
+
+---
+
+Learn more about Histoire [here](https://histoire.dev/).
+</docs>
--- FlOpEDT-front/src/components/qcalendar/HierarchicalColumnFilter.vue
+++ FlOpEDT-front/src/components/qcalendar/HierarchicalColumnFilter.vue
@@ -5,9 +5,9 @@
   <div class="wrapper" :style="styleContainer()">
     <template v-for="node in grid">
       <div class="item" :style="styleItem(node)" @click="toggle(node.id)">
-          <slot name="item" v-bind="{nodeId: node.id, active: indexOf($props.activeIds, node.id) > -1}">
-              id: {{ node.id }}
-          </slot>
+        <slot name="item" v-bind="{ nodeId: node.id, active: indexOf($props.activeIds, node.id) > -1 }">
+          id: {{ node.id }}
+        </slot>
       </div>
     </template>
   </div>
@@ -20,12 +20,12 @@ import { Tree, LinkIdUp } from '@/ts/tree'
 import { forEach, map, filter, values, indexOf, cloneDeep, difference, find } from 'lodash'
 
 const props = defineProps<{
-    activeIds: Array<number>
-    flatNodes: Array<LinkIdUp>
+  activeIds: Array<number>
+  flatNodes: Array<LinkIdUp>
 }>()
 
 const emits = defineEmits<{
-    (e: "update:activeIds", activeIds: Array<number>): void
+  (e: 'update:activeIds', activeIds: Array<number>): void
 }>()
 
 const hierarchy = computed(() => {
@@ -40,13 +40,12 @@ function updateActiveIds(newValue: Array<number>) {
   if (props.activeIds.length != newValue.length) {
     hasChanged = true
   } else {
-    if (find(props.activeIds,
-    (nid, i) => (props.activeIds[i] != newValue[i])) !== undefined) {
+    if (find(props.activeIds, (nid, i) => props.activeIds[i] != newValue[i]) !== undefined) {
       hasChanged = true
     }
   }
   if (hasChanged) {
-    emits("update:activeIds", newValue)
+    emits('update:activeIds', newValue)
   }
 }
 
@@ -55,15 +54,15 @@ const grid = computed(() => {
     return []
   }
 
-  const cellList : Array<GridCell> = []
+  const cellList: Array<GridCell> = []
 
-  const queue : Array<IdX> = [{id: hierarchy.value.root.id, xmin: 0}]
+  const queue: Array<IdX> = [{ id: hierarchy.value.root.id, xmin: 0 }]
   while (queue.length != 0) {
-    const cur = (queue.pop() as IdX)
+    const cur = queue.pop() as IdX
     let curx = cur.xmin
     const tnode = hierarchy.value.byId[cur.id]
-    forEach(tnode?.children, child => {
-      queue.push({id: child.id, xmin:curx})
+    forEach(tnode?.children, (child) => {
+      queue.push({ id: child.id, xmin: curx })
       curx += child.nLeaves
     })
 
@@ -75,18 +74,16 @@ const grid = computed(() => {
       ymax: tnode.depthMax,
     })
   }
-  return(cellList)
+  return cellList
 })
 
-
-function styleItem(node : GridCell) {
-  return("grid-area: " + (node.ymin + 1) + " / " + (node.xmin + 1) + " / "
-        + (node.ymax + 2) + " / " + (node.xmax + 1))
+function styleItem(node: GridCell) {
+  return 'grid-area: ' + (node.ymin + 1) + ' / ' + (node.xmin + 1) + ' / ' + (node.ymax + 2) + ' / ' + (node.xmax + 1)
 }
 
 function styleContainer() {
   return {
-    gridTemplateColumns: 'repeat(' + hierarchy.value.root?.nLeaves + ', 1fr)'
+    gridTemplateColumns: 'repeat(' + hierarchy.value.root?.nLeaves + ', 1fr)',
   }
 }
 
