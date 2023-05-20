44779199f77de8f1b8d3dda1c76fc21ee5d386cb
PRG <prenaud@irit.fr>
2023-05-20 12:38:06 +0200

    feat(multi-parent)! in story and component order in props MATTERS

--- FlOpEDT-front/src/components/hierarchicalFilter/HierarchicalColumnFilter.story.vue
+++ FlOpEDT-front/src/components/hierarchicalFilter/HierarchicalColumnFilter.story.vue
@@ -44,6 +44,28 @@
         <button @click="deactivate(nid)">Deactivate {{ find(useCase4.flatNodes, (n) => n.id == nid)?.name }}</button>
       </template>
     </Variant>
+    <Variant title="Not a tree">
+      <HierarchicalColumnFilter v-model:active-ids="activeNodeIds" :flatNodes="useCase5.flatNodes">
+        <!-- @update:activeIds="updateInPlace"> -->
+        <template #item="{ nodeId, active }">
+          <div :class="['node', active ? 'ac' : 'nac']">
+            {{ find(useCase5.flatNodes, (n) => n.id === nodeId)?.name }}
+            {{ active }}
+          </div>
+        </template>
+      </HierarchicalColumnFilter>
+    </Variant>
+    <Variant title="Almost a tree">
+      <HierarchicalColumnFilter v-model:active-ids="activeNodeIds" :flatNodes="useCase6.flatNodes">
+        <!-- @update:activeIds="updateInPlace"> -->
+        <template #item="{ nodeId, active }">
+          <div :class="['node', active ? 'ac' : 'nac']">
+            {{ find(useCase6.flatNodes, (n) => n.id === nodeId)?.name }}
+            {{ active }}
+          </div>
+        </template>
+      </HierarchicalColumnFilter>
+    </Variant>
   </Story>
 </template>
 
@@ -55,53 +77,79 @@ import { ref } from 'vue'
 const useCase1 = {
   title: 'Named with slots',
   flatNodes: [
-    { id: 5, name: 'id5', parentId: 1 },
-    { id: 1, name: 'id1', parentId: null },
-    { id: 2, name: 'id2', parentId: 1 },
-    { id: 8, name: 'id8', parentId: 5 },
+    { id: 1, name: 'id1', parentsId: [] },
+    { id: 5, name: 'id5', parentsId: [1] },
+    { id: 8, name: 'id8', parentsId: [5] },
+    { id: 2, name: 'id2', parentsId: [1] },
   ],
 }
 
 const useCase2 = [
-  { id: 5, name: 'id5', parentId: 1 },
-  { id: 1, name: 'id1', parentId: null },
-  { id: 2, name: 'id2', parentId: 1 },
-  { id: 3, name: 'id3', parentId: 1 },
-  { id: 8, name: 'id8', parentId: 5 },
-  { id: 9, name: 'id9', parentId: 5 },
+  { id: 1, name: 'id1', parentsId: [] },
+  { id: 5, name: 'id5', parentsId: [1] },
+  { id: 8, name: 'id8', parentsId: [5] },
+  { id: 9, name: 'id9', parentsId: [5] },
+  { id: 2, name: 'id2', parentsId: [1] },
+  { id: 3, name: 'id3', parentsId: [1] },
 ]
 const useCase3 = [
-  { id: 1, name: 'CE', parentId: null },
-  { id: 11, name: 'TD1', parentId: 1 },
-  { id: 12, name: 'TD2', parentId: 1 },
-  { id: 13, name: 'TD3', parentId: 1 },
-  { id: 111, name: 'TP1A', parentId: 11 },
-  { id: 112, name: 'TP1B', parentId: 11 },
-  { id: 121, name: 'TP2A', parentId: 12 },
-  { id: 122, name: 'TP2B', parentId: 12 },
-  { id: 131, name: 'TP3A', parentId: 13 },
-  { id: 132, name: 'TP3B', parentId: 13 },
+  { id: 1, name: 'CE', parentsId: [] },
+  { id: 11, name: 'TD1', parentsId: [1] },
+  { id: 12, name: 'TD2', parentsId: [1] },
+  { id: 13, name: 'TD3', parentsId: [1] },
+  { id: 111, name: 'TP1A', parentsId: [11] },
+  { id: 112, name: 'TP1B', parentsId: [11] },
+  { id: 121, name: 'TP2A', parentsId: [12] },
+  { id: 122, name: 'TP2B', parentsId: [12] },
+  { id: 131, name: 'TP3A', parentsId: [13] },
+  { id: 132, name: 'TP3B', parentsId: [13] },
 ]
 
 const activeNodeIds = ref([131, 121])
 
 const useCase4 = {
   flatNodes: [
-    { id: 1, name: 'CE', parentId: null },
-    { id: 11, name: 'TD1', parentId: 1 },
-    { id: 12, name: 'TD2', parentId: 1 },
-    { id: 13, name: 'TD3', parentId: 1 },
-    { id: 111, name: 'TP1A', parentId: 11 },
-    { id: 112, name: 'TP1B', parentId: 11 },
-    { id: 121, name: 'TP2A', parentId: 12 },
-    { id: 122, name: 'TP2B', parentId: 12 },
-    { id: 131, name: 'TP3A', parentId: 13 },
-    { id: 132, name: 'TP3B', parentId: 13 },
+    { id: 1, name: 'CE', parentsId: [] },
+    { id: 11, name: 'TD1', parentsId: [1] },
+    { id: 12, name: 'TD2', parentsId: [1] },
+    { id: 13, name: 'TD3', parentsId: [1] },
+    { id: 111, name: 'TP1A', parentsId: [11] },
+    { id: 112, name: 'TP1B', parentsId: [11] },
+    { id: 121, name: 'TP2A', parentsId: [12] },
+    { id: 122, name: 'TP2B', parentsId: [12] },
+    { id: 131, name: 'TP3A', parentsId: [13] },
+    { id: 132, name: 'TP3B', parentsId: [13] },
   ],
 }
 
 function deactivate(id: number) {
   activeNodeIds.value = filter(activeNodeIds.value, (nid) => nid != id)
+}
+
+const useCase5 = {
+  flatNodes: [
+    { id: 1, name: 'CE1', parentsId: [] },
+    { id: 2, name: 'CE2A', parentsId: [] },
+    { id: 22, name: 'CE2B', parentsId: [2] },
+    { id: 12, name: 'CE', parentsId: [1, 22] },
+    { id: 121, name: 'TD1', parentsId: [12] },
+    { id: 122, name: 'TD2A', parentsId: [12] },
+    { id: 1222, name: 'TD2B', parentsId: [122] },
+    { id: 132, name: 'TPE', parentsId: [121, 1222] },
+  ],
+}
+
+const useCase6 = {
+  flatNodes: [
+    { id: 1, name: 'CE', parentsId: [] },
+    { id: 11, name: 'TD1', parentsId: [1] },
+    { id: 12, name: 'TD2', parentsId: [1] },
+    { id: 13, name: 'TD3', parentsId: [1] },
+    { id: 111, name: 'TP1', parentsId: [11] },
+    { id: 112, name: 'TP12', parentsId: [11, 12] },
+    { id: 123, name: 'TP23', parentsId: [12, 13] },
+    { id: 133, name: 'TP3', parentsId: [13] },
+  ],
 }
 </script>
 
@@ -148,6 +196,8 @@ take into account in `props.activeNodeIds` the leaf nodes. The inconsistent
 list in the caller component is changed by the `HierarchicalColumnFilter`
 component into a consistent list.
 
+### Several parents
+
 ## User interface
 
 - Click on a node => toggle activation (except specific case where all nodes are active)
@@ -158,6 +208,12 @@ component into a consistent list.
 - The aesthetics should be improved (gaps, remove the tiny margins that appear in blue),
   and I guess slots should help.
 - The style may not be clean (I'm quite ignorant for now): should we scope something?
+- A function that finds a suitable order for the groups (so that it is possible to plot the
+  filter)
+  - a restriction could be that:
+    - at most 2 children can have another parents
+    - these 2 children should be first or last
+    - there is no cycle
 
 ---
 
