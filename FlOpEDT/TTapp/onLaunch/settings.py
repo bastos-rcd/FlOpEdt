77e9965f3e50e865981a0fb264841fe25f8e4f4e
PRG <prenaud@irit.fr>
2023-05-23 17:10:28 +0200

    fix(onLaunch) for real

--- FlOpEDT/TTapp/onLaunch/settings.py
+++ FlOpEDT/TTapp/onLaunch/settings.py
@@ -1,7 +1,8 @@
 import json
 import os
 
-SETTINGS_DIR = os.path.join(os.getcwd(),'TTapp/onLaunch/settings.json')
+module_dir = os.path.abspath(__file__)
+SETTINGS_DIR = os.path.join(os.path.dirname(module_dir),'settings.json')
 def settings():
     f = open(SETTINGS_DIR)
     data = json.load(f)
