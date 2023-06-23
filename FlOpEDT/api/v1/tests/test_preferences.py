c317475365a137f098e8fc66fec34bd5cd54d70c
Pablo <pablo.seban@univ-tlse2.fr>
2023-06-23 15:20:07 +0200

    test(api-V1) Preference

--- FlOpEDT/api/v1/tests/test_preferences.py
+++ FlOpEDT/api/v1/tests/test_preferences.py
@@ -3,6 +3,7 @@ from base.models import Department, DefaultWeekUserPreference, UserPreference
 from people.models import Tutor
 from rest_framework.test import APIClient
 from fixtures import department_a, tutor_fs_a
+from base.timing import datetime_to_floptime, date_to_flopday, time_to_floptime
 import datetime
 
 first_may_2023_8AM = datetime.datetime(year=2023, month=5, day=1, hour=8)
@@ -10,62 +11,91 @@ first_may_2023_9AM = datetime.datetime(year=2023, month=5, day=1, hour=9)
 first_may_2023_10AM = datetime.datetime(year=2023, month=5, day=1, hour=10)
 first_may_2023_11AM = datetime.datetime(year=2023, month=5, day=1, hour=11)
 
+# To be removed when floptime is abandonned
+flop_first_may_2023 = datetime_to_floptime(first_may_2023_8AM)[0]
+
 time_8AM = datetime.time(hour=8)
 time_9AM = datetime.time(hour=9)
 time_10AM = datetime.time(hour=10)
 time_11AM = datetime.time(hour=11)
 time_1PM = datetime.time(hour=13)
 
+# To be removed when floptime is abandonned
+flop_time_8AM = time_to_floptime(time_8AM)
+flop_time_9AM = time_to_floptime(time_9AM)
+flop_time_10AM = time_to_floptime(time_10AM)
+flop_time_11AM = time_to_floptime(time_11AM)
+flop_time_1PM = time_to_floptime(time_1PM)
+
 
 @pytest.fixture
 def user_preference1(db, tutor_fs_a: Tutor) -> UserPreference:
     return UserPreference.objects.create(user=tutor_fs_a,
                                          value=4,
-                                         start_time=first_may_2023_8AM,
-                                         end_time=first_may_2023_9AM)
+                                         start_time=flop_time_8AM,
+                                         duration=60,
+                                         week=flop_first_may_2023.week,
+                                         day=flop_first_may_2023.day,
+                                         # To be changed when floptime is abandonned
+                                         # start_time=first_may_2023_8AM,
+                                         # end_time=first_may_2023_9AM
+                                         )
 
 
 @pytest.fixture
 def user_preference2(db, tutor_fs_a: Tutor) -> UserPreference:
     return UserPreference.objects.create(user=tutor_fs_a,
                                          value=0,
-                                         start_time=first_may_2023_9AM,
-                                         end_time=first_may_2023_10AM)
+                                         start_time=flop_time_9AM,
+                                         duration=60,
+                                         week=flop_first_may_2023.week,
+                                         day=flop_first_may_2023.day,
+                                        #  start_time=first_may_2023_9AM,
+                                        #  end_time=first_may_2023_10AM
+                                        )
 
 
 @pytest.fixture
 def user_preference3(db, tutor_fs_a: Tutor) -> UserPreference:
     return UserPreference.objects.create(user=tutor_fs_a,
                                          value=8,
-                                         start_time=first_may_2023_10AM,
-                                         end_time=first_may_2023_11AM)
+                                         start_time=flop_time_10AM,
+                                         duration=60,
+                                         week=flop_first_may_2023.week,
+                                         day=flop_first_may_2023.day,
+                                        #  start_time=first_may_2023_10AM,
+                                        #  end_time=first_may_2023_11AM
+                                        )
 
 
 @pytest.fixture
 def default_user_preference1(db, tutor_fs_a: Tutor) -> UserPreference:
-    return DefaultWeekUserPreference.objects.create(user=tutor_fs_a,
-                                                    value=8,
-                                                    week_day='m',
-                                                    start_time=time_8AM,
-                                                    end_time=time_10AM)
+    return UserPreference.objects.create(user=tutor_fs_a,
+                                         value=8,
+                                         week=None,
+                                         week_day='m',
+                                         start_time=flop_time_8AM,
+                                         end_time=flop_time_10AM)
 
 
 @pytest.fixture
 def default_user_preference2(db, tutor_fs_a: Tutor) -> UserPreference:
-    return DefaultWeekUserPreference.objects.create(user=tutor_fs_a,
-                                                    value=8,
-                                                    week_day='m',
-                                                    start_time=time_10AM,
-                                                    end_time=time_1PM)
+    return UserPreference.objects.create(user=tutor_fs_a,
+                                         value=8,
+                                         week=None,
+                                         week_day='m',
+                                         start_time=flop_time_10AM,
+                                         end_time=flop_time_1PM)
 
 
 @pytest.fixture
 def default_user_preference3(db, tutor_fs_a: Tutor) -> UserPreference:
     return DefaultWeekUserPreference.objects.create(user=tutor_fs_a,
                                                     value=8,
+                                                    week=None,
                                                     week_day='m',
-                                                    start_time=time_10AM,
-                                                    end_time=time_11AM)
+                                                    start_time=flop_time_10AM,
+                                                    end_time=flop_time_11AM)
 
 
 @pytest.fixture
