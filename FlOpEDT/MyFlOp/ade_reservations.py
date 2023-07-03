05cceee353d46bf679762322bbc99daca710a380
Pablo <pablo.seban@univ-tlse2.fr>
2023-07-03 16:55:14 +0200

    feat(ade) better ade

--- FlOpEDT/MyFlOp/ade_reservations.py
+++ FlOpEDT/MyFlOp/ade_reservations.py
@@ -1,3 +1,29 @@
+# coding: utf-8
+# -*- coding: utf-8 -*-
+
+# This file is part of the FlOpEDT/FlOpScheduler project.
+# Copyright (c) 2017
+# Authors: Iulian Ober, Paul Renaud-Goud, Pablo Seban, et al.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License along with this program. If not, see
+# <http://www.gnu.org/licenses/>.
+#
+# You can be released from the requirements of the license by purchasing
+# a commercial license. Buying such a license is mandatory as soon as
+# you develop activities involving the FlOpEDT/FlOpScheduler software
+# without disclosing the source code of your own applications.
+
 import csv
 import datetime
 from base.models import Room
@@ -7,9 +33,10 @@ from people.models import User
 
 
 @transaction.atomic
-def import_ade_reservations_from_tomorrow():
+def import_ade_reservations_from_tomorrow(ade_reservations_filename=None):
     tomorrow = datetime.date.today() + datetime.timedelta(days=1)
-    ade_reservations_filename = f"/var/flopedt/tmp/misc/Export_Salles_utf8.csv"
+    if ade_reservations_filename is None:
+        ade_reservations_filename = f"/var/flopedt/tmp/misc/Export_Salles_utf8.csv"
     import_reservations_from_ade(filename=ade_reservations_filename, from_date=tomorrow)
 
 
