5220b15655d22dda3794cd8a47769602f44df0f0
Pablo <pablo.seban@univ-tlse2.fr>
2023-07-05 22:29:22 +0200

    fix(planif) debug after_type comments in extract planif

--- FlOpEDT/configuration/extract_planif_file.py
+++ FlOpEDT/configuration/extract_planif_file.py
@@ -254,11 +254,11 @@ def ReadPlanifWeek(department, book, feuille, week, courses_to_stabilize=None):
         except Exception as e:
             raise Exception(f"Exception ligne {row}, semaine {week.nb} de {feuille}: {e} \n")
 
-        # Add after_type dependecies
-        for id, courses_queryset, n in after_type_dependencies:
-            course2 = Course.objects.get(id=id)
-            for course1 in courses_queryset[:n]:
-                    P = Dependency.objects.create(course1=course1, course2=course2)        
+    # Add after_type dependecies
+    for id, courses_queryset, n in after_type_dependencies:
+        course2 = Course.objects.get(id=id)
+        for course1 in courses_queryset[:n]:
+            P = Dependency.objects.create(course1=course1, course2=course2)        
 
 
 @transaction.atomic
