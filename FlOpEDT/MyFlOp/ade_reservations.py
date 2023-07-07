7f9a769d82c676ebc3e7d6e68014c19eacfac1f0
Pablo <pablo.seban@univ-tlse2.fr>
2023-07-07 12:18:57 +0200

    feat(ics) import ade reservations via ics url

--- FlOpEDT/MyFlOp/ade_reservations.py
+++ FlOpEDT/MyFlOp/ade_reservations.py
@@ -30,6 +30,12 @@ from base.models import Room
 from roomreservation.models import RoomReservation, RoomReservationType
 from django.db import transaction
 from people.models import User
+from ics import Calendar
+import requests
+
+ade_reservations_filename = f"/var/flopedt/tmp/misc/Export_Salles_utf8.csv"
+ade_reservations_ics_url = "https://sedna.univ-fcomte.fr/jsp/custom/ufc/cal.jsp?data=7d2be45f7963012e7330cb059c72f77f1c3c057a13954fcb73e210929d5c5728c6412a77b23057dfc03c0942972f2bb1de5b64a61bcf70e2db430bbabcd5338c57066d130b9a7621faf9c42a9c2ef5fc74ed3b5838e481265e30c5a102f3790c16e4212ea9e129e77f2e06fc8dfba77830a5f72847b0c2c00c79892a4ff650a79975b678970c7724d3997a83ca8b8c5b1e3e63b85b4fe4163e5444e84e51c091,1"
+
 
 
 @transaction.atomic
@@ -37,13 +43,52 @@ def import_ade_reservations_from_tomorrow(ade_reservations_filename=None):
     tomorrow = datetime.date.today() + datetime.timedelta(days=1)
     if ade_reservations_filename is None:
         ade_reservations_filename = f"/var/flopedt/tmp/misc/Export_Salles_utf8.csv"
-    import_reservations_from_ade(filename=ade_reservations_filename, from_date=tomorrow)
+    import_reservations_from_ade_csv_file(csv_filename=ade_reservations_filename, from_date=tomorrow)
 
 
 @transaction.atomic
-def import_reservations_from_ade(filename,
-                                 from_date=None,
-                                 to_date=None):
+def import_reservations_from_ade_ics_url(ade_reservations_ics_url=ade_reservations_ics_url, 
+                                         future_only=True):
+    responsible = User.objects.get_or_create(username='ADE')[0]
+    reservation_type = RoomReservationType.objects.get_or_create(name='ADE')[0]
+    room_reservations_to_delete = RoomReservation.objects.filter(reservation_type=reservation_type, 
+                                                                 responsible=responsible)
+    if future_only:
+        tomorrow = datetime.date.today() + datetime.timedelta(days=1)
+        room_reservations_to_delete = room_reservations_to_delete.filter(date__gte=tomorrow)
+    
+    room_reservations_to_delete.delete()
+    calendar = Calendar(requests.get(ade_reservations_ics_url).text)
+    for e in calendar.events:
+        date = e.begin.date()
+        if future_only:
+            if date < tomorrow:
+                continue
+        concateneted_room_names = e.location
+        room_names = concateneted_room_names.split(',')
+        rooms = set()
+        for room_name in room_names:
+            room = Room.objects.get_or_create(name=room_name)[0]
+            rooms.add(room)
+        start_time = e.begin.time()
+        end_time = e.end.time()
+        description = e.description.split('(')[0]
+        title = "ADE"
+        for room in rooms:
+            RoomReservation.objects.create(room=room, 
+                                           reservation_type=reservation_type, 
+                                           date=date, 
+                                           start_time=start_time, 
+                                           end_time=end_time,
+                                           title=title,
+                                           responsible=responsible,
+                                           description=description) 
+
+
+@transaction.atomic
+def import_reservations_from_ade_csv_file(csv_filename,
+                                         from_date=None,
+                                         to_date=None):
     responsible = User.objects.get_or_create(username='ADE')[0]
     reservation_type = RoomReservationType.objects.get_or_create(name='ADE')[0]
     room_reservations_to_delete = RoomReservation.objects.filter(reservation_type=reservation_type, 
@@ -53,8 +98,8 @@ def import_reservations_from_ade(filename,
     if to_date is not None:
         room_reservations_to_delete = room_reservations_to_delete.filter(date__lte=to_date)
     room_reservations_to_delete.delete()
-    with open(filename, newline='') as csvfile:
-        csv_reader = csv.reader(csvfile, delimiter=';', quotechar='|')
+    with open(csv_filename, newline='') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=';', quotechar='|')
         next(csv_reader)
         for row in csv_reader:
             if not row:
