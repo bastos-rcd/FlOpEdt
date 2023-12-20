aa224bf3496a2e8ee8cb47c86562de8837763d12
csl0779a <leo.cusseau@master-developpement-logiciel.fr>
2023-12-20 02:15:15 +0100

    feat(data) test column store

--- FlOpEDT-front/src/stores/display/column.ts
+++ FlOpEDT-front/src/stores/display/column.ts
@@ -1,7 +1,8 @@
 import { CalendarColumn } from '@/components/calendar/declaration'
-import { defineStore } from 'pinia'
+import { defineStore, storeToRefs } from 'pinia'
 import { computed } from 'vue'
 import { useGroupStore } from '@/stores/timetable/group'
+import { Group } from '../declarations'
 
 /**
  * This store is a work in progress,
@@ -11,12 +12,16 @@ import { useGroupStore } from '@/stores/timetable/group'
  */
 export const useColumnStore = defineStore('column', () => {
   const groupStore = useGroupStore()
+  const { fetchedGroups } = storeToRefs(groupStore)
 
   const columns = computed(() => {
     //const totalWeight = groups.value.filter(g => g.columnIds.length === 1).length
     let columns: CalendarColumn[] = []
+    const groupsWithColumns = populateGroupsColumnIds(fetchedGroups.value)
+    console.log('groups: ', fetchedGroups.value)
+    console.log('transformed: ', groupsWithColumns)
     let max: number = 0
-    groupStore.groups.forEach((g) => {
+    groupsWithColumns.forEach((g) => {
       if (g.columnIds.length === 1) {
         columns.push({ id: g.id, name: g.name, weight: 1 })
         if (max < g.id) max = g.id
@@ -25,6 +30,38 @@ export const useColumnStore = defineStore('column', () => {
     columns.push({ id: max + 1, name: 'Avail', weight: 1 })
     return columns
   })
+
+  function populateGroupsColumnIds(groups: Group[]): Group[] {
+    // Map to keep track of children for each group
+    const childrenMap = new Map<number, number[]>()
+
+    // Initialize the map with empty arrays for each group
+    groups.forEach((group) => {
+      childrenMap.set(group.id, [])
+    })
+
+    // Populate the map with children IDs
+    groups.forEach((group) => {
+      if (group.parentsId) {
+        group.parentsId.forEach((parentId) => {
+          const children = childrenMap.get(parentId)
+          if (children) {
+            children.push(group.id)
+          }
+        })
+      }
+    })
+    // Generate columnIds for each group
+    return groups.map((group) => {
+      const children = childrenMap.get(group.id)
+      if (children && children.length > 0) {
+        return { ...group, columnIds: children }
+      } else {
+        // For groups with no children, columnIds will contain only their own ID
+        return { ...group, columnIds: [group.id] }
+      }
+    })
+  }
 
   return {
     columns,
