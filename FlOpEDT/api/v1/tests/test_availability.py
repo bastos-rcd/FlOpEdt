766f0eeb36602f2b17750940f7377e906974ef74
PRG <prenaud@irit.fr>
2024-02-07 10:34:53 +0100

    feat(api-test) some cleaning and orga

--- FlOpEDT/api/v1/tests/test_availability.py
+++ FlOpEDT/api/v1/tests/test_availability.py
@@ -1,6 +1,6 @@
 import pytest
-from rest_framework.test import APIClient
-from urllib.parse import urlencode
+from rest_framework.test import APIClient, APITestCase
+import json
 
 
 @pytest.fixture
@@ -8,21 +8,23 @@ def client():
     return APIClient()
 
 
-def test_bknews(client, availability_a_fs_a_2024_01, tutor_fs_a):
-    endpoint = f"/fr/api/v1/availability/user-actual"
+class TestAvailability:
+    endpoint = f"/fr/api/v1/availability/user-actual/"
 
-    user_id = tutor_fs_a.id
+    def test_test(self, client, availability_a_fs_a_2024_01, tutor_fs_a):
 
-    params = urlencode(
-        {"from_date": "2024-01-01", "to_date": "2024-01-07", "user_id": user_id}
-    )
+        user_id = tutor_fs_a.id
 
-    response = client.get(f"{endpoint}/?{params}")
-    print(response.data)
-    assert False
-    # assert response.status_code == 200
-    # print(response.data)
-    # assert len(response.data) == 1
-    # result = dict(response.data[0])
-    # print(result)
-    # assert result["week"] == 11
+        param_dict = {
+            "from_date": "2024-01-01",
+            "to_date": "2024-01-07",
+            "user_id": user_id,
+        }
+
+        response = client.get(self.endpoint, param_dict)
+
+        assert response.status_code == 200
+        print(response)
+        print(response.content)
+        print(json.loads(response.content))
+        assert False
