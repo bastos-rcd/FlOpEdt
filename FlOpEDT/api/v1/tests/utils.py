25effe4016e392485fc4017f62d06050f2aae239
PRG <prenaud@irit.fr>
2024-02-12 11:03:05 +0100

    feat(api-test) helper

---
+++ FlOpEDT/api/v1/tests/utils.py
@@ -0,0 +1,14 @@
+import json
+from rest_framework import status
+
+
+def retrieve_elements(response, n=None):
+    assert status.is_success(
+        response.status_code
+    ), f"Unsuccessful request (status {response.status_code})"
+    response_dict = json.loads(response.content)
+    if n is not None:
+        assert (
+            len(response_dict) == n
+        ), f"Wrong number of elements: expected {n} got {len(response_dict)}"
+    return response_dict
