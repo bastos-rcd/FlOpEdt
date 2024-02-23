2e716bff9c419137372f4fe10aa638ee63f2a154
Pablo <pablo.seban@univ-tlse2.fr>
2024-02-23 16:22:38 +0100

    feat(new-time) is_default attribute to filter availabilities

--- FlOpEDT/base/models/availability.py
+++ FlOpEDT/base/models/availability.py
@@ -3,17 +3,16 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 from base.timing import Day, str_slot, days_index, days_list
 
-from datetime import date, time, timedelta, datetime
-
+import datetime as dt 
 
 class Availability(models.Model):
-    start_time = models.DateTimeField(default=datetime(1871, 3, 18))
-    date = models.DateField(default=date(1, 1, 1))
-    duration = models.DurationField(default=timedelta(0))
-
+    start_time = models.DateTimeField(default=dt.datetime(1871, 3, 18))
+    date = models.DateField(default=dt.date(1, 1, 1))
+    duration = models.DurationField(default=dt.timedelta(0))
     value = models.SmallIntegerField(
         validators=[MinValueValidator(0), MaxValueValidator(8)], default=8
     )
+    is_default = models.BooleanField(null=True)
 
     class Meta:
         abstract = True
@@ -22,6 +21,8 @@ class Availability(models.Model):
         force_date = kwargs.pop("force_date") if "force_date" in kwargs else False
         if force_date is False:
             self.date = self.start_time.date()
+        if self.date <= dt.date(1,1,7):
+            self.is_default = True
         super(Availability, self).save(*args, **kwargs)
 
     @property
@@ -54,6 +55,13 @@ class Availability(models.Model):
             return self.start_time > other.end_time
         else:
             raise NotImplementedError
+    
+    def weekday_is(self, week_day):
+        return days_list[self.date.weekday()] == week_day
+    
+    def weekday__in(self, week_days):
+        return days_list[self.date.weekday()] in week_days
+    
 
 
 class UserAvailability(Availability):
