7c6eabc41004b24e8283037252783a3f6b8daae5
csl0779a <leo.cusseau@univ-tlse3.fr>
2024-02-29 12:02:16 +0100

    fix(calendar) avail column id computed with all groups

--- FlOpEDT-front/src/stores/display/column.ts
+++ FlOpEDT-front/src/stores/display/column.ts
@@ -14,24 +14,29 @@ export const useColumnStore = defineStore('column', () => {
   const groupStore = useGroupStore()
   const { fetchedStructuralGroups } = storeToRefs(groupStore)
   const groupsSelected = ref<Group[]>([])
-
+  const max = computed((): number => {
+    let currentMax: number = 0
+    fetchedStructuralGroups.value.forEach((g: Group) => {
+      if (g.columnIds.length === 1) {
+        if (currentMax < g.id) currentMax = g.id
+      }
+    })
+    return currentMax
+  })
   const columns = computed(() => {
     let columns: CalendarColumn[] = []
-    let max: number = 0
-    fetchedStructuralGroups.value.forEach((g) => {
+    fetchedStructuralGroups.value.forEach((g: Group) => {
       if (groupsSelected.value.length !== 0) {
         groupsSelected.value.forEach((gs: Group) => {
           if (g.id === gs.id && g.columnIds.length === 1) {
             columns.push({ id: g.id, name: g.name, weight: 1 })
-            if (max < g.id) max = g.id
           }
         })
       } else if (g.columnIds.length === 1) {
         columns.push({ id: g.id, name: g.name, weight: 1 })
-        if (max < g.id) max = g.id
       }
     })
-    columns.push({ id: max + 1, name: 'Avail', weight: 1 })
+    columns.push({ id: max.value + 1, name: 'Avail', weight: 1 })
     return columns
   })
 
