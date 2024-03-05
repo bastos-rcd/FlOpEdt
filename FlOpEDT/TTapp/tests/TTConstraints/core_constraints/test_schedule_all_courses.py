cdac0306f4448322a36fd6fbe65e07e39a5c0549
Pablo <pablo.seban@univ-tlse2.fr>
2024-03-05 14:03:21 +0100

    feat(tests) minor

--- FlOpEDT/TTapp/tests/TTConstraints/core_constraints/test_schedule_all_courses.py
+++ FlOpEDT/TTapp/tests/TTConstraints/core_constraints/test_schedule_all_courses.py
@@ -11,4 +11,5 @@ def test_test(create_typical_situation):
     time2 = dt.datetime(2024,1,1, 10, 0, 0)
     ScheduledCourse.objects.create(course=courses[0], start_time=time1, work_copy=0)
     ScheduledCourse.objects.create(course=courses[1], start_time=time2,  work_copy=0)
-    ScheduleAllCourses(department=courses[0].module.train_prog.department).is_satisfied_for(period, 0)
+    with pytest.raises(AssertionError):
+        ScheduleAllCourses().is_satisfied_for(period, 0)
