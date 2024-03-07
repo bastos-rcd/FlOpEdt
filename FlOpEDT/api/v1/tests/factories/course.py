119afd0ed8be35f39ca1392a2272ab5d5ee73329
PRG <prenaud@irit.fr>
2024-03-07 10:30:56 +0100

    feat(api) course-related factories

---
+++ FlOpEDT/api/v1/tests/factories/course.py
@@ -0,0 +1,50 @@
+import factory
+
+import datetime as dt
+
+from base.models import GenericGroup, TrainingPeriod, Module, CourseType
+
+from .shared import PostGenerationWithCounter
+
+
+class TrainingPeriodDummyFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = "base.TrainingPeriod"
+
+    # not associated with any SchedulingPeriod
+    name = factory.Sequence(lambda n: f"(Training)Period #{n}")
+
+
+class ModuleFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = "base.Module"
+
+    # missing train_prog
+    abbrev = factory.Sequence(lambda n: f"mod{n:02d}")
+    name = factory.Sequence(lambda n: f"Module #{n:02d}")
+    training_period = factory.LazyFunction(lambda: TrainingPeriod.objects.first())
+
+
+class CourseTypeFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = "base.CourseType"
+
+    name = "Lab"
+
+
+def go_groups(self, step, create, extracted, **kwargs):
+    if not create:
+        return
+    self.groups.add(step.attributes.get("group_helper"))
+
+
+class CourseRRGroup(factory.django.DjangoModelFactory):
+    class Meta:
+        model = "base.Course"
+        exclude = ("group_helper",)
+
+    duration = dt.timedelta(minutes=90)
+    group_helper = factory.Iterator(GenericGroup.objects.all())
+    module = factory.LazyFunction(lambda: Module.objects.first())
+    type = factory.LazyFunction(lambda: CourseType.objects.first())
+    groups = PostGenerationWithCounter(go_groups)
