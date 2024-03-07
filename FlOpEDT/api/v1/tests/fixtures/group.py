2be32c802825f603874c431abb798cfe919ab6b0
PRG <prenaud@irit.fr>
2024-03-07 10:31:13 +0100

    feat(api) dummy fixture for courses

---
+++ FlOpEDT/api/v1/tests/fixtures/group.py
@@ -0,0 +1,23 @@
+import pytest
+
+from ..factories.group import StructuralGroupDummyFactory
+
+
+@pytest.fixture
+def make_classical_structural_groups(db):
+    def from_training_programme(tp):
+        groups = []
+        groups.append(StructuralGroupDummyFactory.create(train_prog=tp, name="CM"))
+        for i in range(2):
+            group = StructuralGroupDummyFactory.create(train_prog=tp, name=f"TD{i}")
+            group.parent_groups.add(groups[0])
+            for j in range(2):
+                subgroup = StructuralGroupDummyFactory.create(
+                    train_prog=tp, name=f"TP{i}{j}", basic=True
+                )
+                subgroup.parent_groups.add(group)
+                groups.append(subgroup)
+            groups.append(group)
+        return groups
+
+    return from_training_programme
---
+++ FlOpEDT/api/v1/tests/test_scheduled_courses.py
@@ -0,0 +1,44 @@
+import datetime as dt
+import copy
+
+import pytest
+
+
+from rest_framework.status import (
+    HTTP_406_NOT_ACCEPTABLE,
+    HTTP_403_FORBIDDEN,
+    is_success,
+)
+from api.v1.tests.utils import retrieve_elements, add_user_permission
+
+from base.models import Course, StructuralGroup
+
+
+class TestScheduledCourseList:
+    endpoint = "/fr/api/v1/base/courses/scheduled_courses"
+
+    def test_fixture(self, make_courses):
+        assert Course.objects.count() == (2**3 - 1) * 2, Course.objects.all()
+
+        cms = StructuralGroup.objects.filter(parent_groups__isnull=True)
+        tps = StructuralGroup.objects.filter(basic=True)
+        tds = set(StructuralGroup.objects.all()) - set(cms) - set(tps)
+        assert len(cms) == 1 and len(tds) == 2 and len(tps) == 4
+
+        assert len([c for c in Course.objects.all() if c.groups.count() != 1]) == 0
+
+        n = 2
+        assert (
+            len(Course.objects.filter(groups__in=cms)) == 2**0 * n
+        ), Course.objects.filter(groups__in=cms)
+        assert (
+            len(Course.objects.filter(groups__in=tds)) == 2**1 * n
+        ), Course.objects.filter(groups__in=tds)
+        assert (
+            len(Course.objects.filter(groups__in=tps)) == 2**2 * n
+        ), Course.objects.filter(groups__in=tps)
+
+        for gp in StructuralGroup.objects.all():
+            assert (
+                len(Course.objects.filter(groups__in=[gp])) == n
+            ), Course.objects.filter(groups__in=[gp])
