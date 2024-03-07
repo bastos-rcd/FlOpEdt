7b3e2710ae0369cebd8da35e994c9b3688906967
PRG <prenaud@irit.fr>
2024-03-07 10:30:02 +0100

    feat(api) post_gen hook injecting factory BuildStep

---
+++ FlOpEDT/api/v1/tests/factories/shared.py
@@ -0,0 +1,8 @@
+import factory
+
+
+class PostGenerationWithCounter(factory.PostGeneration):
+    # no need of evaluate? https://github.com/FactoryBoy/factory_boy/issues/936
+    def call(self, instance, step, context):
+        create = step.builder.strategy == factory.enums.CREATE_STRATEGY
+        return self.function(instance, step, create, context.value, **context.extra)
