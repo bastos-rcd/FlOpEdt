0d6c946c5702ea772bebf91398f9ff30c9d92878
PRG <prenaud@irit.fr>
2024-03-07 10:31:33 +0100

    feat(api) test config

--- FlOpEDT/api/v1/tests/conftest.py
+++ FlOpEDT/api/v1/tests/conftest.py
@@ -4,3 +4,5 @@ from api.v1.tests.fixtures.availability import *
 from api.v1.tests.fixtures.people import *
 from api.v1.tests.fixtures.permission import *
 from api.v1.tests.fixtures.configuration import *
+from api.v1.tests.fixtures.course import *
+from api.v1.tests.fixtures.group import *
