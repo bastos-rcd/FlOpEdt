49aa1a3ca0e39fad91d172c266e3d73b937c5581
PRG <prenaud@irit.fr>
2024-03-12 10:57:00 +0100

    feat(api) course start time tes tskeleton

---
+++ FlOpEDT/api/v1/tests/test_allowed_start_times.py
@@ -0,0 +1,32 @@
+import pytest
+
+from rest_framework.status import (
+    HTTP_406_NOT_ACCEPTABLE,
+    HTTP_403_FORBIDDEN,
+    HTTP_400_BAD_REQUEST,
+    is_success,
+)
+from api.v1.tests.utils import retrieve_elements, add_user_permission
+
+from base.models import Department
+
+
+class TestAllowedStartTimes:
+    endpoint = "/fr/api/v1/constraint/base/course_start_time/"
+
+    def test_unknown_dept(self, db, client):
+        assert Department.objects.count() == 0
+        response = client.get(self.endpoint, {"department_id": 1})
+        assert response.status_code == HTTP_400_BAD_REQUEST, response.content
+
+    @pytest.mark.skip("not yet implemented")
+    def test_filter_dept(self):
+        pass
+
+    def test_perm_read_everybody_allowed(self, client, make_users):
+        response = client.get(self.endpoint)
+        assert is_success(response.status_code), response.content
+        u = make_users(1)[0]
+        client.force_authenticate(u)
+        response = client.get(self.endpoint)
+        assert is_success(response.status_code)
