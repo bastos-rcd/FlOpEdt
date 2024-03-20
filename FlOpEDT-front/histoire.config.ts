327d623952a108704366152e7571af50ac88ff6a
PRG <prenaud@irit.fr>
2024-03-20 16:56:16 +0100

    @cyynthia feat(cfg) histoire and ci

--- FlOpEDT-front/histoire.config.ts
+++ FlOpEDT-front/histoire.config.ts
@@ -4,4 +4,9 @@ import { HstVue } from '@histoire/plugin-vue'
 export default defineConfig({
   setupFile: '/src/histoire.setup.ts',
   plugins: [HstVue()],
+
+  // Make it a pure SPA without router
+  // Allows it to be viewed from CI artifacts and other static places
+  routerMode: 'hash',
+  vite: { base: './' },
 })
