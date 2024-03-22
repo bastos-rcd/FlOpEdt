d6252b335aeddda61b5f82ff142f0bea8e491336
csl0779a <leo.cusseau@univ-tlse3.fr>
2024-03-22 14:42:44 +0100

    quality(undo) adding tests and fixing existing one to new implementation

--- FlOpEDT-front/src/composables/undoredo.spec.ts
+++ FlOpEDT-front/src/composables/undoredo.spec.ts
@@ -6,6 +6,7 @@ import { setActivePinia, createPinia } from 'pinia'
 import { Timestamp } from '@quasar/quasar-ui-qcalendar/dist/types/types'
 import { getDateTime, parseTimestamp, parsed, updateWorkWeek } from '@quasar/quasar-ui-qcalendar'
 import { useAvailabilityStore } from '@/stores/timetable/availability'
+import { AvailabilityData, CourseData } from './declaration'
 
 vi.mock('../utils/api.ts')
 
@@ -33,8 +34,8 @@ describe('undoredo composable', () => {
       workCopy: 1,
     })
     const { availabilities } = storeToRefs(availabilityStore)
-    availabilities.value.set('2022-01-25 14:10', [])
-    availabilities.value.get('2022-01-25 14:10')?.push({
+    availabilities.value.set('2022-01-25', [])
+    availabilities.value.get('2022-01-25')?.push({
       id: 23,
       duration: 120,
       start: parseTimestamp('2022-01-25 14:10') as Timestamp,
@@ -44,175 +45,385 @@ describe('undoredo composable', () => {
     })
   })
 
-  it.skip('historizes an update of a course', () => {
+  it('historizes an update of a course', () => {
     expect.assertions(2)
     const scheduledCourseStore = useScheduledCourseStore()
     const { addUpdateBlock } = useUndoredo()
     const courseToUpdate = scheduledCourseStore.getCourse(65692, '2023-04-25')
-    // addUpdate(
-    //   courseToUpdate!.id as number,
-    //   {
-    //     tutorId: courseToUpdate!.tutorId,
-    //     start: parseTimestamp('2022-01-10 08:20') as Timestamp,
-    //     end: courseToUpdate!.end,
-    //     roomId: courseToUpdate!.room,
-    //     suppTutorIds: courseToUpdate!.suppTutorIds,
-    //     graded: courseToUpdate!.graded,
-    //     roomTypeId: courseToUpdate!.roomTypeId,
-    //     groupIds: courseToUpdate!.groupIds,
-    //   },
-    //   'course'
-    // )
-    // expect(getDateTime(courseToUpdate!.start)).toBe('2022-01-10 08:20')
+    addUpdateBlock([
+      {
+        data: {
+          tutorId: courseToUpdate!.tutorId,
+          start: parseTimestamp('2022-01-10 08:20') as Timestamp,
+          end: courseToUpdate!.end,
+          roomId: courseToUpdate!.room,
+          suppTutorIds: courseToUpdate!.suppTutorIds,
+          graded: courseToUpdate!.graded,
+          roomTypeId: courseToUpdate!.roomTypeId,
+          groupIds: courseToUpdate!.groupIds,
+        },
+        objectId: courseToUpdate!.id as number,
+        type: 'course',
+        operation: 'update',
+      },
+    ])
+    expect(getDateTime(courseToUpdate!.start)).toBe('2022-01-10 08:20')
 
-    // addUpdate(
-    //   courseToUpdate!.id as number,
-    //   {
-    //     tutorId: courseToUpdate!.tutorId,
-    //     start: updateWorkWeek(parsed('2025-01-10 08:15') as Timestamp),
-    //     end: courseToUpdate!.end,
-    //     roomId: courseToUpdate!.room,
-    //     suppTutorIds: [55, 3],
-    //     graded: false,
-    //     roomTypeId: 3,
-    //     groupIds: [422, 623, 552],
-    //   },
-    //   'course'
-    // )
-    // expect(getDateTime(courseToUpdate!.start)).toBe('2025-01-10 08:15')
+    addUpdateBlock([
+      {
+        data: {
+          tutorId: courseToUpdate!.tutorId,
+          start: updateWorkWeek(parsed('2025-01-10 08:15') as Timestamp),
+          end: courseToUpdate!.end,
+          roomId: courseToUpdate!.room,
+          suppTutorIds: [55, 3],
+          graded: false,
+          roomTypeId: 3,
+          groupIds: [422, 623, 552],
+        },
+        objectId: courseToUpdate!.id as number,
+        type: 'course',
+        operation: 'update',
+      },
+    ])
+    expect(getDateTime(courseToUpdate!.start)).toBe('2025-01-10 08:15')
   })
 
-  it.skip('reverts an update of a course', () => {
-    expect.assertions(2)
+  it('reverts an update of a course', () => {
     const scheduledCourseStore = useScheduledCourseStore()
     const { addUpdateBlock, revertUpdateBlock } = useUndoredo()
 
-    // const courseToUpdate = scheduledCourseStore.getCourse(65692, '2023-04-25')
+    const courseToUpdate = scheduledCourseStore.getCourse(65692, '2023-04-25')
 
-    // addUpdate(
-    //   courseToUpdate!.id as number,
-    //   {
-    //     tutorId: courseToUpdate!.tutorId,
-    //     start: parseTimestamp('2022-01-10 08:20') as Timestamp,
-    //     end: courseToUpdate!.end,
-    //     roomId: courseToUpdate!.room,
-    //     suppTutorIds: courseToUpdate!.suppTutorIds,
-    //     graded: courseToUpdate!.graded,
-    //     roomTypeId: courseToUpdate!.roomTypeId,
-    //     groupIds: courseToUpdate!.groupIds,
-    //   },
-    //   'course'
-    // )
+    addUpdateBlock([
+      {
+        data: {
+          tutorId: courseToUpdate!.tutorId,
+          start: parseTimestamp('2022-01-10 08:20') as Timestamp,
+          end: courseToUpdate!.end,
+          roomId: courseToUpdate!.room,
+          suppTutorIds: courseToUpdate!.suppTutorIds,
+          graded: courseToUpdate!.graded,
+          roomTypeId: courseToUpdate!.roomTypeId,
+          groupIds: courseToUpdate!.groupIds,
+        },
+        objectId: courseToUpdate!.id as number,
+        type: 'course',
+        operation: 'update',
+      },
+    ])
 
-    // expect(getDateTime(courseToUpdate!.start)).toBe('2022-01-10 08:20')
+    revertUpdateBlock()
 
-    // revertUpdate()
-
-    // expect(getDateTime(courseToUpdate!.start)).toBe('2023-04-25 14:15')
+    expect(getDateTime(courseToUpdate!.start)).toBe('2023-04-25 14:15')
   })
 
-  it.todo('reverts several updates of a course', () => {
-    expect.assertions(4)
+  it('reverts several updates of a course', () => {
     const scheduledCourseStore = useScheduledCourseStore()
     const { addUpdateBlock, revertUpdateBlock } = useUndoredo()
 
-    // const courseToUpdate = scheduledCourseStore.getCourse(65692, '2023-04-25')
-    // addUpdate(
-    //   courseToUpdate!.id as number,
-    //   {
-    //     tutorId: courseToUpdate!.tutorId,
-    //     start: updateWorkWeek(parsed('2022-01-10 08:20') as Timestamp),
-    //     end: courseToUpdate!.end,
-    //     roomId: courseToUpdate?.room || -1,
-    //     suppTutorIds: courseToUpdate!.suppTutorIds,
-    //     graded: courseToUpdate!.graded,
-    //     roomTypeId: courseToUpdate!.roomTypeId,
-    //     groupIds: courseToUpdate!.groupIds,
-    //   },
-    //   'course'
-    // )
+    const courseToUpdate = scheduledCourseStore.getCourse(65692, '2023-04-25')
+    addUpdateBlock([
+      {
+        data: {
+          tutorId: courseToUpdate!.tutorId,
+          start: updateWorkWeek(parsed('2022-01-10 08:20') as Timestamp),
+          end: courseToUpdate!.end,
+          roomId: courseToUpdate?.room || -1,
+          suppTutorIds: courseToUpdate!.suppTutorIds,
+          graded: courseToUpdate!.graded,
+          roomTypeId: courseToUpdate!.roomTypeId,
+          groupIds: courseToUpdate!.groupIds,
+        },
+        objectId: courseToUpdate!.id as number,
+        type: 'course',
+        operation: 'update',
+      },
+    ])
+    addUpdateBlock([
+      {
+        data: {
+          tutorId: courseToUpdate!.tutorId,
+          start: updateWorkWeek(parsed('2025-01-10 08:15') as Timestamp),
+          end: courseToUpdate!.end,
+          roomId: courseToUpdate?.room || -1,
+          suppTutorIds: [],
+          graded: false,
+          roomTypeId: -1,
+          groupIds: [],
+        },
+        objectId: courseToUpdate!.id as number,
+        type: 'course',
+        operation: 'update',
+      },
+    ])
+    revertUpdateBlock()
 
-    // expect(getDateTime(courseToUpdate!.start)).toBe('2022-01-10 08:20')
+    expect(getDateTime(courseToUpdate!.start)).toBe('2022-01-10 08:20')
 
-    // addUpdate(
-    //   courseToUpdate!.id as number,
-    //   {
-    //     tutorId: courseToUpdate!.tutorId,
-    //     start: updateWorkWeek(parsed('2025-01-10 08:15') as Timestamp),
-    //     end: courseToUpdate!.end,
-    //     roomId: courseToUpdate?.room || -1,
-    //     suppTutorIds: [],
-    //     graded: false,
-    //     roomTypeId: -1,
-    //     groupIds: [],
-    //   },
-    //   'course'
-    // )
+    revertUpdateBlock()
 
-    // expect(getDateTime(courseToUpdate!.start)).toBe('2025-01-10 08:15')
-
-    // revertUpdate()
-
-    // expect(getDateTime(courseToUpdate!.start)).toBe('2022-01-10 08:20')
-
-    // revertUpdate()
-
-    // expect(getDateTime(courseToUpdate!.start)).toBe('2023-04-25 14:15')
+    expect(getDateTime(courseToUpdate!.start)).toBe('2023-04-25 14:15')
   })
 
-  it.todo('historizes an update of an availability', () => {
+  it('historizes an update of an availability', () => {
     const availabilityStore = useAvailabilityStore()
     const { addUpdateBlock } = useUndoredo()
     const availToUpdate = availabilityStore.getAvailability(23)
-    // addUpdate(
-    //   availToUpdate!.id as number,
-    //   {
-    //     start: parseTimestamp('2022-01-25 14:00') as Timestamp,
-    //     value: 1,
-    //     duration: 60,
-    //   },
-    //   'availability'
-    // )
-    // expect(getDateTime(availToUpdate!.start)).toBe('2022-01-25 14:00')
-    // expect(availToUpdate!.value).toBe(1)
-    // expect(availToUpdate!.duration).toBe(60)
+    addUpdateBlock([
+      {
+        data: {
+          start: parseTimestamp('2022-01-25 14:00') as Timestamp,
+          value: 1,
+          duration: 60,
+        },
+        objectId: availToUpdate!.id as number,
+        type: 'availability',
+        operation: 'update',
+      },
+    ])
+    expect(getDateTime(availToUpdate!.start)).toBe('2022-01-25 14:00')
+    expect(availToUpdate!.value).toBe(1)
+    expect(availToUpdate!.duration).toBe(60)
 
-    // addUpdate(
-    //   availToUpdate!.id as number,
-    //   {
-    //     start: parseTimestamp('2023-04-22 16:00') as Timestamp,
-    //     value: 7,
-    //     duration: 150,
-    //   },
-    //   'availability'
-    // )
-    // expect(getDateTime(availToUpdate!.start)).toBe('2023-04-22 16:00')
-    // expect(availToUpdate!.value).toBe(7)
-    // expect(availToUpdate!.duration).toBe(150)
+    addUpdateBlock([
+      {
+        objectId: availToUpdate!.id as number,
+        data: {
+          start: parseTimestamp('2023-04-22 16:00') as Timestamp,
+          value: 7,
+          duration: 150,
+        },
+        type: 'availability',
+        operation: 'update',
+      },
+    ])
+    expect(getDateTime(availToUpdate!.start)).toBe('2023-04-22 16:00')
+    expect(availToUpdate!.value).toBe(7)
+    expect(availToUpdate!.duration).toBe(150)
   })
 
-  it.todo('reverts an update of an availability', () => {
+  it('reverts an update of an availability', () => {
     const availabilityStore = useAvailabilityStore()
     const { addUpdateBlock, revertUpdateBlock } = useUndoredo()
     const availToUpdate = availabilityStore.getAvailability(23)
 
-    // addUpdate(
-    //   availToUpdate!.id as number,
-    //   {
-    //     start: parseTimestamp('2022-01-25 14:00') as Timestamp,
-    //     value: 1,
-    //     duration: 60,
-    //   },
-    //   'availability'
-    // )
-    // expect(getDateTime(availToUpdate!.start)).toBe('2022-01-25 14:00')
-    // expect(availToUpdate!.value).toBe(1)
-    // expect(availToUpdate!.duration).toBe(60)
-    // revertUpdate()
-    // expect(getDateTime(availToUpdate!.start)).toBe('2022-01-25 14:10')
-    // expect(availToUpdate!.value).toBe(3)
-    // expect(availToUpdate!.duration).toBe(120)
+    addUpdateBlock([
+      {
+        objectId: availToUpdate!.id as number,
+        data: {
+          start: parseTimestamp('2022-01-25 14:00') as Timestamp,
+          value: 1,
+          duration: 60,
+        },
+        type: 'availability',
+        operation: 'update',
+      },
+    ])
+    revertUpdateBlock()
+    expect(getDateTime(availToUpdate!.start)).toBe('2022-01-25 14:10')
+    expect(availToUpdate!.value).toBe(3)
+    expect(availToUpdate!.duration).toBe(120)
   })
 
-  it.todo('reverts several updates of an availability', () => {})
+  it('reverts several updates of an availability', () => {
+    const availabilityStore = useAvailabilityStore()
+    const { addUpdateBlock, revertUpdateBlock } = useUndoredo()
+    const availToUpdate = availabilityStore.getAvailability(23)
+    addUpdateBlock([
+      {
+        data: {
+          start: parseTimestamp('2022-01-25 14:00') as Timestamp,
+          value: 1,
+          duration: 60,
+        },
+        objectId: availToUpdate!.id as number,
+        type: 'availability',
+        operation: 'update',
+      },
+    ])
+    addUpdateBlock([
+      {
+        data: {
+          start: parseTimestamp('2022-01-25 08:00') as Timestamp,
+          value: 0,
+          duration: 90,
+        },
+        objectId: availToUpdate!.id as number,
+        type: 'availability',
+        operation: 'update',
+      },
+    ])
+    revertUpdateBlock()
+    expect(getDateTime(availToUpdate!.start)).toBe('2022-01-25 14:00')
+    expect(availToUpdate!.value).toBe(1)
+    expect(availToUpdate!.duration).toBe(60)
+    revertUpdateBlock()
+    expect(getDateTime(availToUpdate!.start)).toBe('2022-01-25 14:10')
+    expect(availToUpdate!.value).toBe(3)
+    expect(availToUpdate!.duration).toBe(120)
+  })
+
+  it('Stores Blocks of Availability updates', () => {
+    const availabilityStore = useAvailabilityStore()
+    const { addUpdateBlock } = useUndoredo()
+    const availToUpdate = availabilityStore.getAvailability(23)
+    addUpdateBlock([
+      {
+        objectId: availToUpdate!.id as number,
+        data: {
+          start: parseTimestamp('2022-01-25 14:00') as Timestamp,
+          value: 1,
+          duration: 60,
+        },
+        type: 'availability',
+        operation: 'update',
+      },
+      {
+        data: {
+          start: parseTimestamp('2022-01-25 12:00') as Timestamp,
+          value: 8,
+          duration: 30,
+        },
+        objectId: availToUpdate!.id as number,
+        type: 'availability',
+        operation: 'update',
+      },
+    ])
+    expect(availToUpdate!.value).toBe(8)
+    expect(getDateTime(availToUpdate!.start)).toBe('2022-01-25 12:00')
+    expect(availToUpdate!.duration).toBe(30)
+    addUpdateBlock([
+      {
+        objectId: availToUpdate!.id as number,
+        data: {
+          start: parseTimestamp('2022-01-25 10:00') as Timestamp,
+          value: 5,
+          duration: 25,
+        },
+        type: 'availability',
+        operation: 'update',
+      },
+      {
+        data: {
+          start: parseTimestamp('2022-01-25 09:00') as Timestamp,
+          value: 6,
+          duration: 200,
+        },
+        objectId: availToUpdate!.id as number,
+        type: 'availability',
+        operation: 'update',
+      },
+      {
+        data: {
+          start: parseTimestamp('2022-01-25 07:00') as Timestamp,
+          value: 2,
+          duration: 100,
+        },
+        objectId: availToUpdate!.id as number,
+        type: 'availability',
+        operation: 'update',
+      },
+    ])
+    expect(availToUpdate!.value).toBe(2)
+    expect(getDateTime(availToUpdate!.start)).toBe('2022-01-25 07:00')
+    expect(availToUpdate!.duration).toBe(100)
+  })
+
+  it('reverts blocks of Availability updates', () => {
+    const availabilityStore = useAvailabilityStore()
+    const { addUpdateBlock, revertUpdateBlock } = useUndoredo()
+    const availToUpdate = availabilityStore.getAvailability(23)
+    const updates: {
+      data: CourseData | AvailabilityData
+      objectId: number
+      type: 'course' | 'availability'
+      operation: 'update' | 'create' | 'remove'
+    }[] = []
+    updates.push({
+      objectId: availToUpdate!.id as number,
+      data: {
+        start: parseTimestamp('2022-01-25 14:00') as Timestamp,
+        value: 1,
+        duration: 60,
+      },
+      type: 'availability',
+      operation: 'update',
+    })
+    updates.push({
+      data: {
+        start: parseTimestamp('2022-01-25 12:00') as Timestamp,
+        value: 8,
+        duration: 30,
+      },
+      objectId: availToUpdate!.id as number,
+      type: 'availability',
+      operation: 'update',
+    })
+    addUpdateBlock(updates)
+    addUpdateBlock([
+      {
+        objectId: availToUpdate!.id as number,
+        data: {
+          start: parseTimestamp('2022-01-25 10:00') as Timestamp,
+          value: 5,
+          duration: 25,
+        },
+        type: 'availability',
+        operation: 'update',
+      },
+      {
+        data: {
+          start: parseTimestamp('2022-01-25 09:00') as Timestamp,
+          value: 6,
+          duration: 200,
+        },
+        objectId: availToUpdate!.id as number,
+        type: 'availability',
+        operation: 'update',
+      },
+      {
+        data: {
+          start: parseTimestamp('2022-01-25 07:00') as Timestamp,
+          value: 2,
+          duration: 100,
+        },
+        objectId: availToUpdate!.id as number,
+        type: 'availability',
+        operation: 'update',
+      },
+    ])
+    revertUpdateBlock()
+    expect(availToUpdate!.value).toBe(8)
+    expect(getDateTime(availToUpdate!.start)).toBe('2022-01-25 12:00')
+    expect(availToUpdate!.duration).toBe(30)
+    revertUpdateBlock()
+    expect(availToUpdate!.value).toBe(3)
+    expect(getDateTime(availToUpdate!.start)).toBe('2022-01-25 14:10')
+    expect(availToUpdate!.duration).toBe(120)
+  })
+  it('historizes the creation of an availability', () => {
+    const availabilityStore = useAvailabilityStore()
+    const { availabilities } = storeToRefs(availabilityStore)
+    const { addUpdateBlock } = useUndoredo()
+    addUpdateBlock([
+      {
+        objectId: 23,
+        data: {
+          start: parseTimestamp('2022-01-25 10:00') as Timestamp,
+          value: 5,
+          duration: 25,
+        },
+        type: 'availability',
+        operation: 'create',
+      },
+    ])
+    const newAvail = availabilityStore.getAvailability(1)
+    const oldAvail = availabilityStore.getAvailability(23)
+    const noAvail = availabilityStore.getAvailability(55)
+    expect(availabilities.value.get('2022-01-25')?.length).toBe(2)
+    expect(newAvail).toBeDefined()
+    expect(oldAvail).toBeDefined()
+    expect(noAvail).toBeUndefined()
+  })
 })
