f4ea8220025bdb326dbcf153f3b14a841bdff4f9
PRG <prenaud@irit.fr>
2024-03-25 08:59:40 +0100

    fix(cfg) remove bootstrap

--- FlOpEDT-front/vite.config.ts
+++ FlOpEDT-front/vite.config.ts
@@ -10,7 +10,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
     },
   },
   server: {
--- FlOpEDT-front/vitest.config.ts
+++ FlOpEDT-front/vitest.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
     },
   },
 })
