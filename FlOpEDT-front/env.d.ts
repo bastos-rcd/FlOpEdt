0e6a48dd1cf710cce6f352620ce890b17526c114
csl0779a <leo.cusseau@univ-tlse3.fr>
2024-03-26 15:24:04 +0100

    quality() env.d.ts updated

--- FlOpEDT-front/env.d.ts
+++ FlOpEDT-front/env.d.ts
@@ -1,6 +1,4 @@
 /// <reference types="@histoire/plugin-vue/components" />
-declare module 'multi-range-slider-vue'
-declare module 'bootstrap'
 declare module '@quasar/quasar-ui-qcalendar/src/index.js'
 declare module '@quasar/quasar-ui-qcalendar/src/QCalendarDay.js'
 declare module '@quasar/quasar-ui-qcalendar/src/QCalendarResource.js'
