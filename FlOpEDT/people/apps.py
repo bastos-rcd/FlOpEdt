be4cb8c83373ee8527e444eb6665b36042eebf46
PRG <prenaud@irit.fr>
2024-03-28 09:16:21 +0100

    fix(lint) signals

--- FlOpEDT/people/apps.py
+++ FlOpEDT/people/apps.py
@@ -6,3 +6,6 @@ from django.apps import AppConfig
 
 class PeopleConfig(AppConfig):
     name = "people"
+
+    def ready(self):
+        import people.signals  # pylint: disable=import-outside-toplevel, unused-import
