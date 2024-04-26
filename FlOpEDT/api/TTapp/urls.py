735140ba218ab26d341a643aed4bd76b3fa88769
Pablo <pablo.seban@univ-tlse2.fr>
2024-04-26 13:42:11 +0200

    fix(api) debug urls api/TTapp

--- FlOpEDT/api/TTapp/urls.py
+++ FlOpEDT/api/TTapp/urls.py
@@ -37,7 +37,7 @@ routerTTapp = routers.SimpleRouter()
 # routerTTapp.register(r'simultaneouscourses', views.TTSimultaneousCoursesViewSet)
 # routerTTapp.register(r'limitedstarttimechoices', views.TTLimitedStartTimeChoicesViewSet) # TODO: Fix
 routerTTapp.register(
-    r"constraint", views.FlopConstraintListViewSet, basename="constraints"
+    r"constraint", views.FlopConstraintListViewSet, basename="constraint"
 )
 routerTTapp.register(
     "constraint/(?P<name>\w+)", views.FlopConstraintViewSet, basename="constraint"
