48922fb7dd294bbac8943658f9cffba5ea8dbf98
PRG <prenaud@irit.fr>
2024-05-22 08:01:13 +0200

    fix(login) TMP simple Django login/out

--- FlOpEDT/people/urls.py
+++ FlOpEDT/people/urls.py
@@ -42,6 +42,8 @@ urlpatterns = [
         name="login",
     ),
     path("logout/", auth_views.LogoutView.as_view(next_page="/"), name="logout"),
+    path("login-vue/", views.login_vue_view, name="login-vue"),
+    path("logout-vue/", views.logout_vue_view, name="logout-vue"),
     path(
         "password-reset/",
         auth_views.PasswordResetView.as_view(
